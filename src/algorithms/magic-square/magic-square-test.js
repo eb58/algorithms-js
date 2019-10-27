38035ed0c4536127f8cc1cf332acccc35bf20a72
erich brandl <erich.brandl@gmail.com>
2019-10-27 09:53:57 +0100

    ebtree + first test

---
+++ src/algorithms/magic-square/magic-square-test.js
@@ -0,0 +1,3 @@
+    const magicSquare = require('./magic-square-1');
+    const res = magicSquare(4);
+    console.log(res.length)
--- test/magic-square.test.js
+++ test/magic-square.test.js
@@ -1,5 +1,5 @@
-test('testset magic-square perm', () => {
-    const magicSquare = require('../src/algorithms/magic-square-2');
+test('testset magic-square-2 perm', () => {
+    const magicSquare = require('../src/algorithms/magic-square/magic-square-2');
     const res = magicSquare.magicSquare3x3( [1,2,3,4,5,6,7,8,9] )
     expect(res.length).toEqual(8);
 });
@@ -7,28 +7,28 @@ test('testset magic-square perm', () => {
 ///// 
 
 test('testset magic-square-1 3x3', () => {
-    const magicSquare = require('../src/algorithms/magic-square-1');
+    const magicSquare = require('../src/algorithms/magic-square/magic-square-1');
     debugger;
     const res = magicSquare(3);
     expect(res.length).toEqual(4);
 });
 
 test('testset magic-square-1 4x4', () => {
-    const magicSquare = require('../src/algorithms/magic-square-1');
+    const magicSquare = require('../src/algorithms/magic-square/magic-square-1');
     debugger;
     const res = magicSquare(4);
     expect(res.length).toEqual(880*2);
 });
 
 test('testset magic-square-1 5x5', () => {
-    const magicSquare = require('../src/algorithms/magic-square-1');
+    const magicSquare = require('../src/algorithms/magic-square/magic-square-1');
     // expect( magicSquare(5).length).toEqual(880*2);
 });
 
 
 ///// 
 test('testset magic-square-3 4x4', () => {
-    const magicSquare = require('../src/algorithms/magic-square-3');
+    const magicSquare = require('../src/algorithms/magic-square/magic-square-3');
     debugger;
     const res = magicSquare(4);
     expect(res.length).toEqual(4);
