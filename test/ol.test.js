30a84249e3603950df9d9f4c01d961fc84dd1a4a
A403163 <A403163@10.92.12.37>
2019-12-22 07:09:56 +0100

    ol some tests

--- test/ol.test.js
+++ test/ol.test.js
@@ -1,9 +1,22 @@
 /* global expect */
 const ol = require('../src/ol/ol');
+test('testset simple', () => {
+    expect(ol.sqr(3)).toEqual(9);
+    expect(ol.abs(2)).toEqual(2);
+    expect(ol.abs(-2)).toEqual(2);
+    expect(ol.cub(3)).toEqual(27);
+});
+
 test('testset fac', () => {
     expect(ol.fac(1)).toEqual(1);
     expect(ol.fac(2)).toEqual(2);
     expect(ol.fac(3)).toEqual(6);
     expect(ol.fac(4)).toEqual(24);
+});
+
+test('testset fac', () => {
+    expect(ol.fib(3)).toEqual(2);
+    expect(ol.fib(4)).toEqual(3);
+    expect(ol.fib(5)).toEqual(5);
 });
 
