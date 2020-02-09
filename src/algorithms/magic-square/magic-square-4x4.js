f4aea017a3468801de402eb6d8b1c6fc82e22932
A403163 <A403163@10.92.12.37>
2020-02-09 18:47:17 +0100

    magic-square optimizitions

--- src/algorithms/magic-square/magic-square-4x4.js
+++ src/algorithms/magic-square/magic-square-4x4.js
@@ -1,138 +1,103 @@
 module.exports = () => {
-
+  /*  
+   0  1  2  3
+   4  5  6  7
+   8  9 10 11
+   12 13 14 15
+   */
   const ol = require('../../ol/ol').ol;
-  const num = require('../../ol/ol').num;
-  const interval = require('../../ol/ol').interval;
-  const array = require('../../ol/ol').array;
   const comb = require('../combinations')[0];
   const perm = require('../perm').perm4;
   const magicSquareUtils = require('./magic-square-utils');
+
   const N = 4;
   const utils = magicSquareUtils(N);
   const MN = utils.computeMagicNumber();
-  const computeAvailable2Sums = availableNumbers => Array.from(new Set(comb(availableNumbers, 2).map(x => x[0] + x[1])));
-  const computeCombis = (square, availableNumbers, rowdef) => rowdef
-            ? comb(availableNumbers, rowdef.row.length)
-            .filter(xs => rowdef.restriction(xs, square, availableNumbers))
-            .map(xs => ({xs, perms: perm(xs)}))
-            : [];
+  const idxNotForNumberOne = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-  const numberOneIsNotInFirstQuad = square => [2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].some(x => square[x] === 1);
+  const computeAvailable2Sums = availableNumbers => comb(availableNumbers, 2).map(x => x[0] + x[1]);
+  const computeCombis = (availableNumbers, rowdef) => comb(availableNumbers, rowdef.row.length);
+  const setRow = (square, row, perm) => row.forEach((x, idx) => square[x] = perm[idx]);
+  const numberOneIsNotInUpperLeft = xs => idxNotForNumberOne.some(x => xs[x] === 1);
+
   const rowsDef = [
-    /*  
-     0  1  2  3
-     4  5  6  7
-     8  9 10 11
-     12 13 14 15
-     */
     {row: [0, 5, 10, 15], restriction: xs => xs.sum() === MN}, // diag1
     {row: [12, 9, 6, 3], restriction: xs => xs.sum() === MN}, // diag2
-    {row: [1, 2], restriction: (xs, square, availableNumbers) => {
-        if (square[0] + xs[0] + xs[1] + square[3] !== MN)
+    {row: [1, 2],
+      restriction: (xs, sq, availableNumbers) => {
+        if (sq[0] + xs[0] + xs[1] + sq[3] !== MN)
           return false;
-        const x59 = MN - square[5] - square[9]
-        if (!availableNumbers.without(x[1]).includes(x59 - xs[0]) && !availableNumbers.without(x[0]).includes(x59 - xs[1]))
-          return false;
-        const x610 = MN - square[6] - square[10]
-        if (!availableNumbers.without(x[1]).includes(x610 - xs[0]) && !availableNumbers.without(x[0]).includes(x610 - xs[1]))
-          return false;
-        return true
-        // console.log( 'AAAA', availableNumbers )
-        const len = availableNumbers.length
-        const min2sum = availableNumbers[0] + availableNumbers[1];
-        const max2sum = availableNumbers[len - 1] + availableNumbers[len - 2];
-        return true &&
-                min2sum + square[0] + square[3] <= MN &&
-                min2sum + square[5] + square[6] <= MN &&
-                min2sum + square[9] + square[10] <= MN &&
-                min2sum + square[12] + square[15] <= MN &&
-                min2sum + square[0] + square[12] <= MN &&
-                min2sum + square[3] + square[15] <= MN &&
-                max2sum + square[0] + square[3] >= MN &&
-                max2sum + square[5] + square[6] >= MN &&
-                max2sum + square[9] + square[10] >= MN &&
-                max2sum + square[12] + square[15] >= MN &&
-                max2sum + square[0] + square[12] >= MN &&
-                max2sum + square[3] + square[15] >= MN
 
+        const x59 = MN - sq[5] - sq[9]
+        const x610 = MN - sq[6] - sq[10]
+
+        const av0 = availableNumbers.without(xs[0]);
+        const av1 = availableNumbers.without(xs[1]);
+
+        if (!av1.includes(x59 - xs[0]) && !av0.includes(x59 - xs[1]))
+          return false;
+
+        if (!av1.includes(x610 - xs[0]) && !av0.includes(x610 - xs[1]))
+          return false;
+
+        return true;
       }
     },
-    {row: [4, 8], restriction: (xs, square, availableNumbers) => {
-        const len = availableNumbers.length
-
-        if (xs[0] + xs[1] + square[0] + square[12] !== MN)
+    {row: [4, 8],
+      restriction: (xs, sq, availableNumbers) => {
+        if (sq[0] + xs[0] + xs[1] + sq[12] !== MN)
           return false;
 
-        if (!availableNumbers.includes(MN - (square[1] + square[5] + square[9])))
+        if (!availableNumbers.includes(MN - (xs[0] + sq[5] + sq[6])) && !availableNumbers.includes(MN - (xs[1] + sq[5] + sq[6])))
           return false;
-        if (!availableNumbers.includes(MN - (square[2] + square[6] + square[10])))
-          return false;
-
-        if (square[1] + square[5] + square[9] + availableNumbers[0] > MN)
-          return false;
-        if (square[2] + square[6] + square[10] + availableNumbers[0] > MN)
+        if (!availableNumbers.includes(MN - (xs[0] + sq[9] + sq[10])) && !availableNumbers.includes(MN - (xs[1] + sq[9] + sq[10])))
           return false;
 
-        if (square[1] + square[5] + square[9] + availableNumbers[len - 1] < MN)
+        if (!availableNumbers.includes(MN - (sq[1] + sq[5] + sq[9])))
           return false;
-        if (square[2] + square[6] + square[10] + availableNumbers[len - 1] < MN)
+        if (!availableNumbers.includes(MN - (sq[2] + sq[6] + sq[10])))
           return false;
 
-        if (!availableNumbers.includes(MN - (xs[0] + square[5] + square[6])) && !availableNumbers.includes(MN - (xs[1] + square[5] + square[6])))
-          return false;
-        if (!availableNumbers.includes(MN - (xs[0] + square[9] + square[10])) && !availableNumbers.includes(MN - (xs[1] + square[9] + square[10])))
-          return false;
         return true;
       }},
-    {row: [13], restriction: (xs, square, availableNumbers) => xs[0] + square[1] + square[5] + square[9] === MN},
-    {row: [14], restriction: (xs, square, availableNumbers) => xs[0] + square[2] + square[6] + square[10] === MN},
-    {row: [7], restriction: (xs, square, availableNumbers) => xs[0] + square[4] + square[5] + square[6] === MN},
-    {row: [11], restriction: (xs, square, availableNumbers) => xs[0] + square[8] + square[9] + square[10] === MN},
-            //{row: [4], restriction: (xs, square, availableNumbers) => xs[0] + square[5] + square[6] + square[7] === MN},
-            // {row: [8], restriction: (xs, square, availableNumbers) => xs[0] + square[9] + square[10] + square[11] === MN},
+    {row: [13], restriction: (xs, sq) => xs[0] + sq[1] + sq[5] + sq[9] === MN},
+    {row: [14], restriction: (xs, sq) => xs[0] + sq[2] + sq[6] + sq[10] === MN},
+    {row: [7], restriction: (xs, sq) => xs[0] + sq[4] + sq[5] + sq[6] === MN},
+    {row: [11], restriction: (xs, sq) => xs[0] + sq[8] + sq[9] + sq[10] === MN},
   ]
 
-  const setRow = (square, row, perm) => {
-    row.forEach((x, idx) => {
-      square[x] = perm[idx]
-    });
-    return row.map((x, idx) => x < 0 ? x : perm[idx]);
-  }
-  const res = [];
   const combineToMagicSquare = (square, availableNumbers, i) => {
 
-    if (numberOneIsNotInFirstQuad(square)) {
+    if (numberOneIsNotInUpperLeft(square)) {
       return;
     }
 
-    if (availableNumbers.length === 0 && utils.isMagic(square)) {
+    if (availableNumbers.length === 0) {
       res.push(square);
       return;
     }
 
-    const combis = computeCombis(square, availableNumbers, rowsDef[i]);
- 
-    if (i === 41 && combis.length) {
-      console.log('i:', i, 'combis#:', combis.length, 'available:', availableNumbers.join(' '), '2sums:', computeAvailable2Sums(availableNumbers).sort().join(' '));
-      utils.dump('aaa', square);//, JSON.stringify(x, 0, 3));
-    }
+    const rowDef = rowsDef[i];
+    const combis = computeCombis(availableNumbers, rowDef)
+            .filter(xs => rowDef.restriction(xs, square, availableNumbers))
 
-    // console.log(availableNumbers, JSON.stringify(combis));
+//    if (i === 20)  {
+//      console.log('i:', i, 'combis#:', combis.length, combis, 'available:', availableNumbers.join(' '), 'available2sums:', computeAvailable2Sums(availableNumbers).sort().join(' '));
+//      utils.dump('aaa', square);//, JSON.stringify(x, 0, 3));
+//    }
+
     combis.forEach(combi => {
-      const newAvailableNumbers = availableNumbers.subtract(combi.xs)
-      combi.perms.forEach(perm => {
-        setRow(square, rowsDef[i].row, perm);
+      const newAvailableNumbers = availableNumbers.subtract(combi)
+      perm(combi).forEach(p => {
+        setRow(square, rowDef.row, p);
         combineToMagicSquare([...square], newAvailableNumbers, i + 1)
       })
     })
   }
 
   const square = ol.rangeFilled(N * N, 0);
-  const availableNumbers = ol.range(N * N).map(x => x + 1);
-
-  const x = computeCombis(square, availableNumbers, rowsDef[0]);
-  console.log(x.length, '#', availableNumbers.join(' '));//, JSON.stringify(x, 0, 3));
-
+  const availableNumbers = ol.range(4 * 4).map(x => x + 1);
+  const res = [];
   combineToMagicSquare(square, availableNumbers, 0);
   return res;
 }
--- test/magic-square.2.test.js
+++ test/magic-square.2.test.js
@@ -1,13 +1,13 @@
 /* global expect */
 
 xtest('testset magic-square-3x3', () => {
-    const magicSquare = require('../src/algorithms/magic-square/magic-square-3x3');
-    const res = magicSquare();
-    expect(res.length).toEqual(8);
+  const magicSquare = require('../src/algorithms/magic-square/magic-square-3x3');
+  const res = magicSquare();
+  expect(res.length).toBe(8);
 });
 
 test('testset magic-square-4x4', () => {
-    const magicSquare = require('../src/algorithms/magic-square/magic-square-4x4');
+  const magicSquare = require('../src/algorithms/magic-square/magic-square-4x4');
     const res = magicSquare();
-    expect(res.length).toEqual(1760);
+    expect(res.length).toBe(880);
 });
