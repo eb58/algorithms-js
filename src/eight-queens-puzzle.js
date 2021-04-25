f6d7eec280ea9b7ad87b049ff8e6e5fff2dd231c
eb58 <erich.brandl@gmail.com>
2021-04-25 18:57:16 +0200

    some optimizations

--- src/eight-queens-puzzle.js
+++ src/eight-queens-puzzle.js
@@ -1,48 +1,26 @@
-const N = 8
-const range = (n) => [...Array(n).keys()];
-const ininterval = (x, a, b) => a <= x && x <= b;
-const row = (x) => Math.floor(x / N);
-const col = (x) => x % N;
-const dump = (fld) => range(N).map(r => range(N).map((c) => fld[r * N + c] ? "1" : "0").join(' ') + "\n").join('');
-const connectionSet = (x) => {
-    const res = new Set();
-    const f = (r, c, delta) => { while (ininterval(r, 0, N - 1) && ininterval(c, 0, N - 1)) { res.add(r * N + c);[r, c] = delta(r, c) } }
-    f(row(x), col(x), (r, c) => [r + 0, c + 1])
-    f(row(x), col(x), (r, c) => [r + 1, c + 0])
-    f(row(x), col(x), (r, c) => [r + 0, c - 1])
-    f(row(x), col(x), (r, c) => [r - 1, c + 0])
-    f(row(x), col(x), (r, c) => [r + 1, c + 1])
-    f(row(x), col(x), (r, c) => [r - 1, c + 1])
-    f(row(x), col(x), (r, c) => [r + 1, c - 1])
-    f(row(x), col(x), (r, c) => [r - 1, c - 1])
-    return Array.from(res);
-}
-const CONNECTIONSETS = range(N * N).map(connectionSet);
-
-const hasCollision = (fld, idx) => CONNECTIONSETS[idx].some(x => fld[x] === 1);
-
-const solve = () => {
-    const solv = (fld, row, cols) => {
-        if (row >= N) {
-            res.push([...fld]);
-        }
-        else {
-            cols.filter(x => !hasCollision(fld, row * N + x)).forEach(c => {
-                fld[row * N + c] = 1;
-                //console.log(row, c); console.log(dump(fld));
-                solv(fld, row + 1, cols.filter(x => x != c));
+const solveEightQueens = (N) => {
+    const range = (n) => [...Array(n).keys()];
+    const ininterval = (x, a, b) => a <= x && x <= b;
+    const row = (x) => Math.floor(x / N);
+    const col = (x) => x % N;
+    const connectionSet = (x) => {
+        const f = (r, c, d) => { while (ininterval(r, 0, N - 1) && ininterval(c, 0, N - 1)) { res.add(r * N + c);[r, c] = [r + d[0], c + d[1]] } }
+        const res = new Set();
+        [-1, 0, +1].flatMap(x => [-1, 0, +1].map(y => [x, y])).filter(a => a[0] !== 0 || a[1] !== 0).forEach(d => f(row(x), col(x), d))
+        return Array.from(res);
+    }
+    const CONNECTIONSETS = range(N * N).map(connectionSet);
+    const hasCollision = (fld, idx) => CONNECTIONSETS[idx].some(x => fld[x] === 1);
+    const solve = (fld, row, cols) => {
+        row >= N
+            ? res.push([...fld])
+            : cols.filter(col => !hasCollision(fld, row * N + col)).forEach(c => {
+                fld[row * N + c] = 1
+                solve(fld, row + 1, cols.filter(x => x != c))
                 fld[row * N + c] = 0;
             })
-        }
     }
-
     const res = [];
-    const fld = range(N * N).map(() => 0);
-    solv(fld, 0, range(N));
-    // console.log("BB", res)
-    res.forEach(x => console.log(dump(x)))
+    solve(range(N * N).map(() => 0), 0, range(N));
     return res;
 }
-
-solve()
-
