bd468aa85ae920017a18c4f6d093ba543c9a8ab8
eb58 <erich.brandl@gmail.com>
2021-04-30 17:30:56 +0200

    some simplifications

--- src/eight-queens-puzzle.js
+++ src/eight-queens-puzzle.js
@@ -1,27 +1,23 @@
-const solveEightQueens = (N) => {
+module.exports = solveEightQueens = (N) => {
     const range = (n) => [...Array(n).keys()];
     const ininterval = (x, a, b) => a <= x && x <= b;
+    const infield = (r, c) => ininterval(r, 0, N - 1) && ininterval(c, 0, N - 1);
     const row = (x) => Math.floor(x / N);
     const col = (x) => x % N;
-    const connectionSet = (x) => {
-        const f = (r, c, d) => { while (ininterval(r, 0, N - 1) && ininterval(c, 0, N - 1)) { res.add(r * N + c);[r, c] = [r + d[0], c + d[1]] } }
-        const res = new Set();
-        [-1, 0, +1].flatMap(x => [-1, 0, +1].map(y => [x, y])).filter(a => a[0] !== 0 || a[1] !== 0).forEach(d => f(row(x), col(x), d))
-        return Array.from(res);
+    const diagSet = (x) => {
+        const f = (r, c, d) => { while (infield(r, c)) { res.push(r * N + c);[r, c] = [r + d[0], c + d[1]] } }
+        const res = [];
+        [[-1, +1], [-1, -1]].forEach(delta => f(row(x), col(x), delta))
+        return res;
     }
-    const CONNECTIONSETS = range(N * N).map(connectionSet);
-    const hasCollision = (fld, idx) => CONNECTIONSETS[idx].some(x => fld[x] === 1);
-    const solve = (fld, row, cols) =>
-        row >= N
+    const DIAGSETS = range(N * N).map(diagSet);
+    const hasCollision = (fld, r, c) => DIAGSETS[r * N + c].some(x => fld[x] === 1);
+    const set = (fld, r, c, callback) => { fld[r * N + c] = 1; callback(); fld[r * N + c] = 0; }
+    const solve = (fld, r, cols) =>
+        r >= N
             ? res.push([...fld])
-            : cols.filter(col => !hasCollision(fld, row * N + col)).forEach(c => {
-                fld[row * N + c] = 1
-                solve(fld, row + 1, cols.filter(x => x != c))
-                fld[row * N + c] = 0;
-            })
+            : cols.forEach(c => hasCollision(fld, r, c) || set(fld, r, c, () => solve(fld, r + 1, cols.filter(x => x != c))))
     const res = [];
     solve(range(N * N).map(() => 0), 0, range(N));
     return res;
 }
-
-module.exports = solveEightQueens;
