419fd352ff1a0f7bb1fc94bfb99b47744130eb41
Erich Brandl <erich.brandl@gmail.com>
2021-12-21 10:45:54 +0100

    changes reverted

--- src/magic-square/magic-square-4x4.js
+++ src/magic-square/magic-square-4x4.js
@@ -1,29 +1,23 @@
-Array.prototype.subtract = function (xs) { return this.filter(x => !xs.includes(x)); }
+Array.prototype.subtract = function (xs) { return this.filter(x => !xs.includes(x)) };
 
 module.exports = () => {
   const range = require('../ol').ol.range;
-  const bitset = require('../ol').bitset;
-  const combBS = require('../combinations').combBS;
+  const comb = require('../combinations').comb1;
   const perm = require('../perm').perm4;
 
   const N = 4;
-  const RANGE16 = range(N * N).map((x) => x + 1);
-  const MN = RANGE16.reduce((acc, x) => acc + x, 0) / N;
   const square = range(N * N).map(() => 0);
-  const availableNumbers = bitset.fromArray(RANGE16);
-
+  const availableNumbers = range(N * N).map((x) => x + 1);
+  const MN = availableNumbers.reduce((acc, x) => acc + x, 0) / N;
+  const idxNotForNumberOne = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   const setRow = (square, row, perm) => row.forEach((x, idx) => (square[x] = perm[idx]));
-  const numberOneIsNotInUpperLeft = (() => {
-    const idxNotForNumberOne = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    return (xs) => idxNotForNumberOne.some((x) => xs[x] === 1);
-  })();
-
-  const chk = (avn, s1, s2) => s1 != s2 && bitset.includes(avn, MN - s1) && bitset.includes(avn, MN - s2);
+  const numberOneIsNotInUpperLeft = (xs) => idxNotForNumberOne.some((x) => xs[x] === 1);
+  const chk = (avn, s1, s2) => s1 != s2 && avn.includes(MN - s1) && avn.includes(MN - s2);
   const check = (xs, sq, avn, x1, x2, y1, y2) => chk(avn, xs[0] + sq[x1] + sq[x2], xs[1] + sq[y1] + sq[y2]) || chk(avn, xs[1] + sq[x1] + sq[x2], xs[0] + sq[y1] + sq[y2]);
 
   const rowsDef = [
     { row: [0, 5, 10, 15], restriction: (xs) => xs[0] + xs[1] + xs[2] + xs[3] === MN }, // diag1
-    { row: [12, 9, 6, 3], restriction: (xs) => xs[0] + xs[1] + xs[2] + xs[3] === MN }, // diag2
+    { row: [12, 9, 6, 3], restriction: (xs) => xs[0] + xs[1] + xs[2] + xs[3] === MN }, // diag2 
     { row: [1, 2], restriction: (xs, sq, avn) => sq[0] + xs[0] + xs[1] + sq[3] === MN && check(xs, sq, avn, 5, 9, 6, 10) },
     { row: [4, 8], restriction: (xs, sq, avn) => sq[0] + xs[0] + xs[1] + sq[12] === MN && check(xs, sq, avn, 5, 6, 9, 10) },
     { row: [7], restriction: (xs, sq) => xs[0] + sq[4] + sq[5] + sq[6] === MN },
@@ -37,22 +31,23 @@ module.exports = () => {
       return;
     }
 
-    if (bitset.isEmpty(availableNumbers)) {
+    if (availableNumbers.length === 0) {
       res.push(square);
       return;
     }
 
     const rowDef = rowsDef[i];
-    combBS(availableNumbers, rowDef.row.length)
-      .filter(xs => rowDef.restriction(bitset.toArray(xs), square, availableNumbers))
-      // .map(xs => (i==0 &&  console.log(i, bitset.toArray(xs)), xs))
-      .forEach((combi) => {
-        const newAvailableNumbers = bitset.diff(availableNumbers, combi);
-        perm(bitset.toArray(combi)).forEach((p) => {
+    if (rowDef) {
+      const predicate = (xs) => rowDef.restriction(xs, square, availableNumbers);
+      comb(availableNumbers, rowDef.row.length).filter(predicate).forEach((combi) => {
+        const newAvailableNumbers = availableNumbers.subtract(combi);
+        perm(combi).forEach((p) => {
           setRow(square, rowDef.row, p);
           combineToMagicSquare([...square], newAvailableNumbers, i + 1);
         });
       });
+
+    }
   };
   const res = [];
   combineToMagicSquare(square, availableNumbers, 0);
