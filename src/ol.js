87f858773bcf140dff7982dd961fca5b1e2c75cb
Erich Brandl <erich.brandl@gmail.com>
2022-04-12 19:35:39 +0200

    ol.js reorganized

--- src/ol.js
+++ src/ol.js
@@ -1,3 +1,143 @@
+const id = (x) => x
+const abs = (x) => (x >= 0 ? x : -x)
+const sqr = (x) => x ** 2
+const cube = (x) => x ** 3
+const fac = (x) => range(x).reduce((acc, n) => acc * (n + 1), 1)
+const fib = (x) => (x <= 2 ? 1 : fib(x - 1) + fib(x - 2))
+const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b))
+const add = (a, b) => a + b
+const inc = (x) => x + 1
+const dec = (x) => x - 1
+
+const add2obj = (o, k, v) => ((o[k] = [...(o[k] || []), v]), o)
+const groupBy = (xs, proj) => xs.reduce((a, v) => add2obj(a, proj(v), v), {})
+
+const feedx = (x, f) => f(x)
+const call = (f, ...args) => f(...args)
+
+// predicates
+const eq = (x, y) => x === y
+const lt = (x, y) => x < y
+const gt = (x, y) => x > y
+const odd = (x) => x % 2 !== 0
+const even = (x) => x % 2 === 0
+const ininterval = (x, a, b) => a <= x && x <= b
+const leapyear = (x) => (x % 4 === 0 && x % 100 !== 0) || x % 400 === 0
+
+// generate predicates
+// usage: [1,2,3,4,5].filter(gtPred(3)) // -> [4,5]
+const gtPred = (x) => (y) => y > x
+const ltPred = (x) => (y) => y < x
+
+// combine predicates
+// const { not, and, or, gtPred } = ol;
+// [1,2,3,4,5].filter(not(gtPred(3))) # -> [1,2,3]
+// [1,2,3,4,5].filter(and(gtPred(3),ltPred(5))) # -> [4]
+const not = (f) => (x) => !f(x)
+const and = (f, g) => (x) => f(x) && g(x)
+const or = (f, g) => (x) => f(x) || g(x)
+const xor = (f, g) => (x) => !!(f(x) ^ g(x))
+const comb = (f, g) => (x) => f(g(x))
+const every =
+  (...fs) =>
+  (x) =>
+    fs.every((f) => f(x))
+const some =
+  (...fs) =>
+  (x) =>
+    fs.some((f) => f(x))
+
+// compare
+const cmp = (x, y) => (x === y ? 0 : x < y ? -1 : +1)
+const comparer = (proj) => (x, y) => cmp(proj(x), proj(y))
+const comparerByKey = (key) => comparer((o) => o[key])
+const cmpNumbers = (x, y) => x - y
+
+// arrays
+//
+// range(3)            // [0,1,2]
+// range(5).map(inc)   // [1,2,3,4,5]
+// range(5).map(()=>0) // [0,0,0,0,0]
+const range = (n) => [...Array(n).keys()]
+
+// rangeFilled(3, 0)   // [0, 0, 0]
+const rangeFilled = (n, val) => range(n).map(() => val)
+
+const randomInRange = (min, max) => Math.random() * (max - min) + min
+const randomIntInRange = (min, max) => Math.floor(randomInRange(min, max + 1))
+const randomArray = (n, min, max) => range(n).map(() => randomInRange(min, max))
+const randomIntArray = (n, min, max) => range(n).map(() => randomIntInRange(min, max))
+const sum = (xs) => xs.reduce((acc, x) => acc + x, 0)
+const without = (xs, x) => xs.filter((y) => x !== y)
+const withoutIndex = (xs, idx) => xs.filter((_, i) => i !== idx)
+const sort = (xs, cmp) => (xs.sort(cmp), xs)
+
+// examples
+// zipped  = zip([1,2,3], [4,5,6])  // -> [[1,4],[2,5],[3,6]]
+// sums    = zip([1,2,3], [4,5,6], (a,b)=>a+b) // -> [5,7,9]
+// men   = [{name:'hugo', age:36 }, {name:'hans', age:37 }]
+// women = [{name:'anna', age:35 }, {name:'lena', age:27 }]
+// married = zip(men, women, (a,b) => ({ 'husband': a, 'wife': b }) )
+const zip = (xs, ys, f) => xs.map((x, i) => (f ? f(xs[i], ys[i]) : [xs[i], ys[i]]))
+
+// uniq doesn't work on referenztypes. can we fix this easily?
+const uniq = (xs) => Array.from(new Set(xs))
+
+const clone = (o) => JSON.parse(JSON.stringify(o))
+
+// persons     = [ {name:"Max", age: 59}, {name:"Hans", age: 19}, {name:"Johannes", age: 29}]
+// oldest      = max(persons,p=> p.age) # -> {name:"Max", age: 59}
+// youngest    = min(persons,p=> p.age) # -> {name:"Hans", age: 19}
+// longestName = max(persons,p => p.name.length) # -> {name:"Max", age: 59}
+const max = (xs, proj) => xs.reduce((a, x) => ((proj || id)(x) > (proj || id)(a) ? x : a), xs[0])
+const min = (xs, proj) => xs.reduce((a, x) => ((proj || id)(x) < (proj || id)(a) ? x : a), xs[0])
+
+const interval = (a, b) => ({
+  range: () => [...Array(b - a + 1).keys()].map((x) => x + a),
+  contains: (x) => a <= x && x <= b,
+  intersects: (x, y) => !(y < a || x > b),
+
+  // inc/dec x, but stay in interval
+  inc: (x) => (x >= b ? b : x + 1),
+  dec: (x) => (x <= a ? a : x - 1),
+
+  random: () => randomInRange(a, b),
+  randomInt: () => randomIntInRange(a, b),
+})
+
+// Wrappers
+const num = (x) => ({
+  abs: () => abs(x),
+  sqr: () => sqr(x),
+  cube: () => cube(x),
+  ininterval: (a, b) => ininterval(x, a, b),
+})
+
+const array = (xs) => ({
+  sum: () => sum(xs),
+  without: (x) => without(xs, x),
+  withoutIndex: (idx) => withoutIndex(idx),
+  initial: () => xs.slice(0, -1),
+  head: () => [xs[0]],
+  tail: () => xs.slice(1),
+  rest: () => xs.slice(1),
+  first: () => xs[0],
+  last: () => xs[xs.length - 1],
+  max: () => max(xs),
+  min: () => min(xs),
+  groupBy: (proj) => groupBy(xs, proj),
+  uniq: () => uniq(xs),
+  unite: (ys) => uniq([...xs, ...ys]),
+  xor: (ys) => [...xs, ...ys].filter((x) => !(xs.includes(x) && ys.includes(x))),
+  intersect: (ys) => xs.filter((x) => ys.includes(x)),
+  subtract: (ys) => xs.filter((x) => !ys.includes(x)),
+  subsetOf: (ys) => ys.every((x) => xs.includes(x)),
+  tap: (f) => (f(xs), xs),
+  largerThan: (a) => xs.filter((x) => x > a),
+  smallerThan: (a) => xs.filter((x) => x < a),
+  shuffle: (a) => range(a.length).map,
+})
+
 const bitset = {
   MAX: 32,
   fromArray: (xs) => xs.reduce((acc, x) => acc | (1 << x), 0),
@@ -53,158 +193,13 @@ const bitset = {
   },
 }
 
-const ol = {
-  id: (x) => x,
-  abs: (x) => (x >= 0 ? x : -x),
-  sqr: (x) => x ** 2,
-  cube: (x) => x ** 3,
-  fac: (x) => ol.range(x).reduce((acc, n) => acc * (n + 1), 1),
-  fib: (x) => (x <= 2 ? 1 : ol.fib(x - 1) + ol.fib(x - 2)),
-  gcd: (a, b) => (a % b === 0 ? b : gcd(b, a % b)),
-  add: (a, b) => a + b,
-  sum: (a, b) => a + b,
-  inc: (x) => x + 1,
-  dec: (x) => x - 1,
-
-  randomInRange: (min, max) => Math.random() * (max - min) + min,
-  randomIntInRange: (min, max) => Math.floor(ol.randomInRange(min, max + 1)),
-
-  feedx: (x, f) => f(x),
-  call: (f, ...args) => f(...args),
-
-  // predicates
-  eq: (x, y) => x === y,
-  lt: (x, y) => x < y,
-  gt: (x, y) => x > y,
-  odd: (x) => x % 2 !== 0,
-  even: (x) => x % 2 === 0,
-  ininterval: (x, a, b) => a <= x && x <= b,
-  leapyear: (x) => (x % 4 === 0 && x % 100 !== 0) || x % 400 === 0,
-
-  // generate predicates
-  // usage: [1,2,3,4,5].filter(gtPred(3)) // -> [4,5]
-  gtPred: (x) => (y) => y > x,
-  ltPred: (x) => (y) => y < x,
-
-  // combine predicates
-  // const { not, and, or, gtPred } = ol;
-  // [1,2,3,4,5].filter(not(gtPred(3))) # -> [1,2,3]
-  // [1,2,3,4,5].filter(and(gtPred(3),ltPred(5))) # -> [4]
-  not: (f) => (x) => !f(x),
-  and: (f, g) => (x) => f(x) && g(x),
-  or: (f, g) => (x) => f(x) || g(x),
-  xor: (f, g) => (x) => !!(f(x) ^ g(x)),
-  comb: (f, g) => (x) => f(g(x)),
-  every:
-    (...fs) =>
-    (x) =>
-      fs.every((f) => f(x)),
-  some:
-    (...fs) =>
-    (x) =>
-      fs.some((f) => f(x)),
-
-  // compare
-  cmp: (x, y) => (x === y ? 0 : x < y ? -1 : +1),
-  cmpNumbers: (x, y) => x - y,
-  comparer: (proj) => (x, y) => cmp(proj(x), proj(y)),
-  comparerByKey: (key) => comparer((o) => o[key]),
-
-  //***************************************************** */
-  // arrays
-  //***************************************************** */
-
-  // range(3)              // [0,1,2]
-  // range(5).map(ol.inc)  // [1,2,3,4,5]
-  // range(5).map(()=>0)   // [0,0,0,0,0]
-  range: (n) => [...Array(n).keys()],
-
-  rangeFilled: (n, val) => ol.range(n).map(() => val),
-  randomArray: (n, min, max) => ol.range(n).map(() => ol.randomInRange(min, max)),
-  randomIntArray: (n, min, max) => ol.range(n).map(() => ol.randomIntInRange(min, max)),
-  sum: (xs) => xs.reduce((acc, x) => acc + x, 0),
-  without: (xs, x) => xs.filter((y) => x !== y),
-  withoutIndex: (xs, idx) => xs.filter((_, i) => i !== idx),
-  sort: (xs, cmp) => (xs.sort(cmp), xs),
-
-  // examples
-  // zipped  = zip([1,2,3], [4,5,6])  // -> [[1,4],[2,5],[3,6]]
-  // sums    = zip([1,2,3], [4,5,6], (a,b)=>a+b) // -> [5,7,9]
-  // men   = [{name:'hugo', age:36 }, {name:'hans', age:37 }]
-  // women = [{name:'anna', age:35 }, {name:'lena', age:27 }]
-  // married = zip(men, women, (a,b) => ({ 'husband': a, 'wife': b }) )
-  zip: (xs, ys, f) => xs.map((x, i) => (f ? f(xs[i], ys[i]) : [xs[i], ys[i]])),
-
-  // uniq dont work on referenztypes
-  // can we fix this easyly?
-  uniq: (xs) => Array.from(new Set(xs)),
-
-  clone: (o) => JSON.parse(JSON.stringify(o)),
-  max: (xs, proj) =>
-    proj ? xs.reduce((a, x) => (proj(x) > proj(a) ? x : a), xs[0]) : xs.reduce((a, x) => (x > a ? x : a), xs[0]),
-  min: (xs, proj) =>
-    proj ? xs.reduce((a, x) => (proj(x) < proj(a) ? x : a), xs[0]) : xs.reduce((a, x) => (x < a ? x : a), xs[0]),
-  // persons     = [ {name:"Max", age: 59}, {name:"Hans", age: 19}, {name:"Johannes", age: 29}]
-  // oldest      = max(persons,p=> p.age) # -> {name:"Max", age: 59}
-  // youngest    = min(persons,p=> p.age) # -> {name:"Hans", age: 19}
-  // longestName = max(persons,p => p.name.length) # -> {name:"Max", age: 59}
-  add2obj: (o, k, v) => (o[k] ? o[k].push(v) : (o[k] = [v]), o),
-  groupBy: (xs, proj, transform) => xs.reduce((a, v) => ol.add2obj(a, proj(v), transform ? transform(v) : v), {}),
-}
-
-const interval = (a, b) => ({
-  range: () => [...Array(b - a + 1).keys()].map((x) => x + a),
-  contains: (x) => a <= x && x <= b,
-  intersects: (x, y) => !(y < a || x > b),
-
-  // inc/dec x, but stay in interval
-  inc: (x) => (x >= b ? b : x + 1),
-  dec: (x) => (x <= a ? a : x - 1),
-
-  random: () => ol.randomInRange(a, b),
-  randomInt: () => ol.randomIntInRange(a, b),
-})
-
-// Wrappers
-const num = (x) => ({
-  abs: () => ol.abs(x),
-  sqr: () => ol.sqr(x),
-  cube: () => ol.cube(x),
-  ininterval: (a, b) => ol.ininterval(x, a, b),
-})
-
-const array = (xs) => ({
-  sum: () => ol.sum(xs),
-  without: (x) => ol.without(xs, x),
-  withoutIndex: (idx) => ol.withoutIndex(idx),
-  initial: () => xs.slice(0, -1),
-  head: () => [xs[0]],
-  tail: () => xs.slice(1),
-  rest: () => xs.slice(1),
-  first: () => xs[0],
-  last: () => xs[xs.length - 1],
-  max: () => ol.max(xs),
-  min: () => ol.min(xs),
-  groupBy: (proj, transform) => ol.groupBy(xs, proj, transform),
-  uniq: () => ol.uniq(xs),
-  unite: (ys) => ol.uniq([...xs, ...ys]),
-  xor: (ys) => [...xs, ...ys].filter((x) => !(xs.includes(x) && ys.includes(x))),
-  intersect: (ys) => xs.filter((x) => ys.includes(x)),
-  subtract: (ys) => xs.filter((x) => !ys.includes(x)),
-  subsetOf: (ys) => ys.every((x) => xs.includes(x)),
-  tap: (f) => (f(xs), xs),
-  largerThan: (a) => xs.filter((x) => x > a),
-  smallerThan: (a) => xs.filter((x) => x < a),
-  shuffle: (a) => range(a.length).map,
-})
-
-shuffle = (a) => {
-  let count = a.length
+shuffle = (xs) => {
+  let count = xs.length
   while (count) {
     const randomnumber = (Math.random() * count--) | 0
-    const temp = a[count]
-    a[count] = a[randomnumber]
-    a[randomnumber] = temp
+    const temp = xs[count]
+    xs[count] = xs[randomnumber]
+    xs[randomnumber] = temp
   }
 }
 
@@ -297,6 +292,53 @@ logtor = (f) => {
 }
 
 // experimentell !!!
+
+const ol = {
+  id,
+  abs,
+  sqr,
+  cube,
+  fac,
+  fib,
+  gcd,
+  add,
+  sum,
+  inc,
+  dec,
+  without,
+  withoutIndex,
+  range,
+  rangeFilled,
+  randomArray,
+  randomIntArray,
+  randomInRange,
+  randomIntInRange,
+  cmp,
+  feedx,
+  call,
+  eq,
+  lt,
+  gt,
+  odd,
+  even,
+  ininterval,
+  leapyear,
+  not,
+  or,
+  and,
+  xor,
+  every,
+  some,
+  gtPred,
+  ltPred,
+  add2obj,
+  groupBy,
+  cmp,
+  comparer,
+  comparerByKey,
+  cmpNumbers,
+  sort,
+}
 
 module.exports = {
   ol,
