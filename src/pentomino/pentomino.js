47bb4c2c88c8fead851491bda3fa0741d1ce8e52
Erich Brandl <erich.brandl@gmail.com>
2024-04-30 11:03:14 +0200

    added functions redim, extract , minmaxIndex + tests

--- src/pentomino/pentomino.js
+++ src/pentomino/pentomino.js
@@ -1,8 +1,18 @@
 const range = (n) => [...Array(n).keys()]
+const max = (xs) => xs.reduce((a, x) => x > a ? x : a, xs[0])
+const min = (xs) => xs.reduce((a, x) => x < a ? x : a, xs[0])
+
 // const reshape = (xs, cols) => range(cols).map(c => xs.slice(c * cols, (c + 1) * xs.length / cols))
 const reshape = (xs, dim) => xs.reduce((acc, x, i) => (i % dim ? acc[acc.length - 1].push(x) : acc.push([x])) && acc, [])
 const isInInterval = (xs, a, b) => a <= xs && xs <= b
 
+// matrix function
+const redim = (mat, nrows, ncols, defaultVal = 0) => range(nrows).map(r => range(ncols).map((c) => mat[r] && mat[r][c] || defaultVal))
+const extract = (mat, val, defaultVal = 0) => mat.map(r => r.map(c => c === val ? c : defaultVal))
+const minmaxIndex = (xs,v) => xs.reduce(({ minc, maxc }, cv, idx) => ({ minc: cv !== v ? minc : min([minc, idx]), maxc: cv !== v ? maxc : max([maxc, idx]) }), { minc: 1000, maxc: 0 })
+// const reduceMatrix = (mat, defaultIgnore = 0) = 
+
+// some static data
 const RANGE60 = range(60)
 
 
@@ -15,16 +25,16 @@ b c c c h l l k k j
 b b i i i i i j j j`.trim().replaceAll(' ', '')
 const filledBoard = filledBoardAsString.replaceAll('\n', '').split('')
 
-const cellsWithValue = (board, c) => board.map((val,idx) => ({ r: Math.trunc(idx / 10), c: idx % 10, val })).filter( o => o.val === c)
+const cellsWithValue = (board, c) => board.map((val, idx) => ({ r: Math.trunc(idx / 10), c: idx % 10, val })).filter(o => o.val === c)
 const elements = 'abcdefghijkl'.split('').reduce((acc, c) => [...acc, filledBoard.map(x => x === c ? c : ' ')], [])
 
 const translate = (val, dr, dc) => {
-    const cellswithval = cellsWithValue(filledBoard,val)
+    const cellswithval = cellsWithValue(filledBoard, val)
     const inside = cellswithval.every((o) => isInInterval(o.r + dr, 0, 5) && isInInterval(o.c + dc, 0, 9))
-    console.log( dr, dc, inside, cellswithval)
-    const y = cellswithval.map( o => ({...o, r:o.r+dr, c:o.c+dc }))
-    const x = y.reduce((acc, cell) => (acc[cell.r * 10 + cell.c] = cell.val, acc), range(60).map(()=> ' '))
-    return inside ? x  : undefined
+    // console.log( dr, dc, inside, cellswithval)
+    const y = cellswithval.map(o => ({ ...o, r: o.r + dr, c: o.c + dc }))
+    const x = y.reduce((acc, cell) => (acc[cell.r * 10 + cell.c] = cell.val, acc), range(60).map(() => ' '))
+    return inside ? x : undefined
 }
 
 const rotate90 = (e) => {
@@ -32,20 +42,24 @@ const rotate90 = (e) => {
     const rotated = cellswithval.map(cell => ({ ...cell, r: -cell.c, c: cell.r }))
     const inside = rotated.every((o) => isInInterval(o.r, 0, 5) && isInInterval(o.c, 0, 9))
     return inside ? rotated : undefined
-    //return inside ? e.map((_, idx) => e[idx - offset] || ' ') : undefined
 }
 
-''.split('').forEach(c => {
+'abcdefghijkl'.split('').forEach(c => {
     for (let dr = -5; dr <= 5; dr++) for (let dc = -9; dc <= 9; dc++) {
         const tr = translate(c, dr, dc)
         if (tr) {
             // console.log("AAA", dr, dc, "\n", reshape(tr, 10).map(x => x.join("")))
             if (rotate90(tr)) console.log(rotate90(tr))
 
-            res.push(tr);
         }
     }
 
 })
 
-module.exports = { filledBoard, elements, cellsWithValue, translate, }
+module.exports = { 
+    redim, extract, minmaxIndex, 
+    filledBoard, 
+    elements, 
+    cellsWithValue, 
+    translate
+ }
--- test/pentomino.test.js
+++ test/pentomino.test.js
@@ -1,4 +1,33 @@
-const { filledBoard, elements, cellsWithValue, translate, rotate90 } = require("../src/pentomino/pentomino");
+const {
+    redim,
+    extract,
+    minmaxIndex,
+    filledBoard,
+    elements,
+    cellsWithValue,
+    translate,
+    rotate90
+} = require("../src/pentomino/pentomino");
+
+test('redim matrix', () => {
+    const mat = [[1, 2, 3], [4, 5, 6]]
+    expect(redim(mat, 1, 1)).toEqual([[1]]);
+    expect(redim(mat, 2, 1)).toEqual([[1], [4]]);
+    expect(redim(mat, 2, 2)).toEqual([[1, 2], [4, 5]]);
+    expect(redim(mat, 1, 4)).toEqual([[1, 2, 3, 0]]);
+    expect(redim(mat, 4, 4)).toEqual([[1, 2, 3, 0], [4, 5, 6, 0], [0, 0, 0, 0], [0, 0, 0, 0]]);
+});
+
+test('extract matrix', () => {
+    const mat = [[1, 7, 7, 7], [1, 6, 6, 8], [1, 3, 4, 3], [1, 1, 9, 6]]
+    expect(extract(mat, 1)).toEqual([[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0]]);
+});
+
+test('minmaxIndex', () => {
+    expect(minmaxIndex([0, 1, 0, 0, 1, 0],1)).toEqual({minc:1, maxc:4});
+    expect(minmaxIndex([1, 1, 0, 0, 1, 0],1)).toEqual({minc:0, maxc:4});
+    expect(minmaxIndex([1, 1, 5, 0, 1, 7],1)).toEqual({minc:0, maxc:4});
+});
 
 test('pentomino basics', () => {
     expect(elements.length).toBe(12);
@@ -9,13 +38,13 @@ test('pentomino basics', () => {
 });
 
 test('cellsWithValue', () => {
-    expect(cellsWithValue(filledBoard,'a').length).toBe(5);
-    expect(cellsWithValue(filledBoard,'a')).toEqual([
-        {'c':0, 'r':0, val:'a'},
-        {'c':0, 'r':1, val:'a'},
-        {'c':1, 'r':1, val:'a'},
-        {'c':1, 'r':2, val:'a'},
-        {'c':1, 'r':3, val:'a'},
+    expect(cellsWithValue(filledBoard, 'a').length).toBe(5);
+    expect(cellsWithValue(filledBoard, 'a')).toEqual([
+        { 'c': 0, 'r': 0, val: 'a' },
+        { 'c': 0, 'r': 1, val: 'a' },
+        { 'c': 1, 'r': 1, val: 'a' },
+        { 'c': 1, 'r': 2, val: 'a' },
+        { 'c': 1, 'r': 3, val: 'a' },
     ]);
 })
 
