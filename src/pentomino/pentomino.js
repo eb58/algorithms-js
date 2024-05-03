a58302c6580f4ead7b55c131504a1ee4984c088f
Erich Brandl <erich.brandl@gmail.com>
2024-05-03 17:41:47 +0200

    pentomino: added 4x15

--- src/pentomino/pentomino.js
+++ src/pentomino/pentomino.js
@@ -18,7 +18,7 @@ const reshape = (xs, dim) => xs.reduce((acc, x, i) => (i % dim ? acc[acc.length 
 // matrix functions
 const redim = (mat, nrows, ncols, defVal = 0) => range(nrows).map(r => range(ncols).map((c) => mat[r] && mat[r][c] || defVal))
 const minmaxRowIndex = (mat, v) => mat.reduce((res, row, n) => !row.includes(v) ? res : minmax(res, { min: n, max: n }), { min: 1000, max: -1 })
-const minmaxColIndex = (mat, v) => mat.reduce((res, row) => minmax(res, minmaxColIndexArr(row, v)), { min: 1000, max: 0 })
+const minmaxColIndex = (mat, v) => mat.reduce((res, row) => minmax(res, minmaxColIndexArr(row, v)), { min: 1000, max: -1 })
 const makeQuadratic = (mat, defVal = 0) => redim(mat, Math.max(mat.length, mat[0].length), Math.max(mat.length, mat[0].length), defVal)
 const makeCopy = (mat) => mat.map(r => [...r])
 const transpose = (mat) => mat.map((r, ri) => r.map((_, ci) => mat[ci][ri]))
@@ -44,81 +44,100 @@ const rotate90 = (mat, defVal = 0) => {
 }
 const rotateN90 = (matrix, n) => range(n).reduce(m => rotate90(m, ' '), matrix)
 
-///  PENTOMINO
-const filledBoardAsString = `
-n w w y y y y p p p
-n n w w y u u u p p
-l n t w x u f u z z
-l n t x x x f f z v
-l t t t x f f z z v
-l l i i i i i v v v`.trim().replaceAll(' ', '')
-const filledBoard = filledBoardAsString.replaceAll('\n', '').split('')
-const SYMBOLS = [... new Set(filledBoard)].sort() // ['f', 'i', 'l', 'n', 'p', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const [DIMR, DIMC] = [filledBoardAsString.split('\n').length, filledBoardAsString.split('\n')[0].length] // 6, 10
 
-const extract = (board, val, defVal = ' ') => {
-    const extracted = board.map(r => r.map(c => c === val ? c : defVal))
-    const mmr = minmaxRowIndex(extracted, val)
-    const mmc = minmaxColIndex(extracted, val)
-    const [dimr, dimc] = [mmr.max - mmr.min + 1, mmc.max - mmc.min + 1]
-    return redim([], dimr, dimc, defVal).map((r, ri) => r.map((_, ci) => extracted[mmr.min + ri][mmc.min + ci]), defVal)
+//  PENTOMINO 
+const filledBoards = {
+    "4x15": `
+        l l x n n n i i i i i f v v v
+        l x x x p n n w w z f f f t v
+        l u x u p p w w y z z z f t v
+        l u u u p p w y y y y z t t t`.trim().replaceAll(' ', '').replaceAll('\n', '').split(''),
+    "6x10": `
+        n w w y y y y p p p
+        n n w w y u u u p p
+        l n t w x u f u z z
+        l n t x x x f f z v
+        l t t t x f f z z v
+        l l i i i i i v v v`.trim().replaceAll(' ', '').replaceAll('\n', '').split('')
 }
 
-const translateBoard = (board, dr, dc, defVal = ' ') => {
-    const newBoard = translate(board, dr, dc, defVal)
-    let [cnt1, cnt2] = [0, 0]
-    for (let r = 0; r < DIMR; r++) for (let c = 0; c < DIMC; c++) {
-        cnt1 += (board[r][c] !== defVal)
-        cnt2 += (newBoard[r][c] !== defVal)
+const pentonimo = (filledBoard, DIMR = 6, DIMC = 10) => {
+    const SYMBOLS = [... new Set(filledBoard)].sort() // ['f', 'i', 'l', 'n', 'p', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    // console.log( SYMBOLS, DIMR, DIMC )
+
+    const extract = (board, val, defVal = ' ') => {
+        const extracted = board.map(r => r.map(c => c === val ? c : defVal))
+        const mmr = minmaxRowIndex(extracted, val)
+        const mmc = minmaxColIndex(extracted, val)
+        const [dimr, dimc] = [mmr.max - mmr.min + 1, mmc.max - mmc.min + 1]
+        return redim([], dimr, dimc, defVal).map((r, ri) => r.map((_, ci) => extracted[mmr.min + ri][mmc.min + ci]), defVal)
     }
-    return cnt1 != cnt2 ? undefined : newBoard
-}
 
-const generateTiles = () => {
-    const res = SYMBOLS.reduce((acc, c) => ({ ...acc, [c]: [] }), {})
+    const translateBoard = (board, dr, dc, defVal = ' ') => {
+        const newBoard = translate(board, dr, dc, defVal)
+        let [cnt1, cnt2] = [0, 0]
+        for (let r = 0; r < DIMR; r++) for (let c = 0; c < DIMC; c++) {
+            cnt1 += (board[r][c] !== defVal)
+            cnt2 += (newBoard[r][c] !== defVal)
+        }
+        return cnt1 != cnt2 ? undefined : newBoard
+    }
 
-    return SYMBOLS.reduce((acc, ch) => {
-        const extractSym = makeQuadratic(extract(reshape(filledBoard, 10), ch))
-        const N = ch === 'f' ? 1 : 4 // symmetrie!!
-        const tiles = range(N)
-            .reduce((acc, n) => [...acc, rotateN90(makeCopy(extractSym), n)], [])
-            .reduce((acc, tile) => ([...acc, tile, transpose(tile)]), [])
-            .map(tile => makeQuadratic(extract(tile, ch), ' ').flat().join(''))
-        const uniqTilesAsString = [... new Set(tiles)]
-        const uniqTiles = uniqTilesAsString.map(t => reshape(t.split(''), Math.sqrt(t.length)))
+    const generateTiles = () => {
+        const res = SYMBOLS.reduce((acc, c) => ({ ...acc, [c]: [] }), {})
 
-        uniqTiles.map(tile => redim(tile, DIMR, DIMC, ' ')).forEach(board =>
-            range(DIMR).forEach(dr => range(DIMC).forEach(dc => {
-                const translated = translateBoard(board, dr, dc)
-                if (translated) {
-                    acc[ch] = [...acc[ch], translated]
-                }
-            }))
+        return SYMBOLS.reduce((acc, ch) => {
+            const extractSym = makeQuadratic(extract(reshape(filledBoard, DIMC), ch))
+            const N = ch === 'f' ? 1 : 4 // symmetrie!!
+            const tiles = range(N)
+                .reduce((acc, n) => [...acc, rotateN90(makeCopy(extractSym), n)], [])
+                .reduce((acc, tile) => ([...acc, tile, transpose(tile)]), [])
+                .map(tile => makeQuadratic(extract(tile, ch), ' ').flat().join(''))
+            const uniqTilesAsString = [... new Set(tiles)]
+            const uniqTiles = uniqTilesAsString.map(t => reshape(t.split(''), Math.sqrt(t.length)))
+
+            uniqTiles.map(tile => redim(tile, DIMR, DIMC, ' ')).forEach(board =>
+                range(DIMR).forEach(dr => range(DIMC).forEach(dc => {
+                    const translated = translateBoard(board, dr, dc)
+                    if (translated) {
+                        acc[ch] = [...acc[ch], translated]
+                    }
+                }))
+            )
+            return acc
+        }, res)
+    }
+
+    const solve = () => {
+        const allTiles = generateTiles();
+        const problem = Object.entries(allTiles).reduce((acc, [s, tiles]) => [...acc, ...tiles.map(tile => [
+            ...SYMBOLS.map(ch => s === ch ? 1 : 0),
+            ...tile.flat().map(x => x === ' ' ? 0 : 1)])],
+            [])
+
+        const mapToSymbol = Object.entries(allTiles).reduce((acc, [s, tiles]) => [...acc, ...tiles.map(() => s)], [])
+
+        // const solutions = dlxlib.solve(problem); // ~30 sec
+        // const solutions = dlx_solve(problem, 368 ); // ~10.5 sec
+        const solutions = dancingLinks.findAll(problem.map(row => ({ row }))).map(x => x.map(o => o.index))// ~9.5 sec
+        return solutions.map(solution =>
+            reshape(solution
+                .map(r => problem[r].slice(12).map(y => y === 1 ? mapToSymbol[r] : ''))
+                .reduce((acc, tile) => zip(acc, tile, (a, b) => a || b || ''), range(60).map(() => '')), DIMC)
+                .map(r => r.join('')).join('')
         )
-        return acc
-    }, res)
-}
+    }
 
-// console.log(allTiles)
-
-const solvePentonimo = () => {
-    const allTiles = generateTiles();
-    const problem = Object.entries(allTiles).reduce((acc, [s, tiles]) => [...acc, ...tiles.map(tile => [
-        ...SYMBOLS.map(ch => s === ch ? 1 : 0),
-        ...tile.flat().map(x => x === ' ' ? 0 : 1)])],
-        [])
-
-    const mapToSymbol = Object.entries(allTiles).reduce((acc, [s, tiles]) => [...acc, ...tiles.map(() => s)], [])
-
-    // const solutions = dlxlib.solve(problem); // ~30 sec
-    const solutions = dlx_solve(problem, 2339); // ~10.5 sec
-    // const solutions = dancingLinks.findAll(problem.map(row => ({ row }))).map(x => x.map(o => o.index))// ~9.5 sec
-    return solutions.map(solution =>
-        reshape(solution
-            .map(r => problem[r].slice(12).map(y => y === 1 ? mapToSymbol[r] : ''))
-            .reduce((acc, tile) => zip(acc, tile, (a, b) => a || b || ''), range(60).map(() => '')), 10)
-            .map(r => r.join('')).join('')
-    )
+    return {
+        DIMC, DIMR,
+        solve,
+        internals: {
+            SYMBOLS,
+            extract,
+            translateBoard,
+            generateTiles
+        }
+    }
 }
 
 //const sol = solvePentonimo();
@@ -134,10 +153,8 @@ const solvePentonimo = () => {
 // console.log(answer)
 
 module.exports = {
-    filledBoard,
-    redim, extract,
-    minmaxColIndexArr, minmaxColIndex, minmaxRowIndex,
+    filledBoards,
+    redim, minmaxColIndexArr, minmaxColIndex, minmaxRowIndex,
     reshape, makeQuadratic, transpose, rotate90, translate,
-    generateTiles,
-    solvePentonimo
+    pentonimo,
 }
--- test/pentomino.test.js
+++ test/pentomino.test.js
@@ -1,17 +1,8 @@
 const {
-    redim,
-    minmaxColIndexArr,
-    minmaxColIndex,
-    minmaxRowIndex,
-    reshape,
-    transpose,
-    rotate90,
-    makeQuadratic,
-    extract,
-    translate,
-    filledBoard,
-    generateTiles,
-    solvePentonimo
+    filledBoards,
+    redim, minmaxColIndexArr, minmaxColIndex, minmaxRowIndex,
+    reshape, makeQuadratic, transpose, rotate90, translate,
+    pentonimo
 } = require('../src/pentomino/pentomino');
 
 test('matrix makeQuadratic', () => {
@@ -32,27 +23,28 @@ test('redim matrix', () => {
     expect(redim(mat, 4, 4)).toEqual([[1, 2, 3, 0], [4, 5, 6, 0], [0, 0, 0, 0], [0, 0, 0, 0]]);
 });
 
-test('extract matrix', () => {
-    const mat = [[1, 7, 7, 7], [1, 6, 6, 8], [1, 3, 4, 3], [1, 1, 9, 6]]
-    expect(extract(mat, 1, 0)).toEqual([[1, 0], [1, 0], [1, 0], [1, 1]]);
-});
-
 test('minmaxColIndexArr', () => {
-    const m = reshape(filledBoard.map(x => x === 't' ? 't' : ' '), 10)
     expect(minmaxColIndexArr([0, 1, 0, 0, 1, 0], 1)).toEqual({ min: 1, max: 4 });
     expect(minmaxColIndexArr([1, 1, 0, 0, 1, 0], 1)).toEqual({ min: 0, max: 4 });
     expect(minmaxColIndexArr([1, 1, 5, 0, 1, 7], 1)).toEqual({ min: 0, max: 4 });
-    expect(minmaxColIndexArr(m[4], 't')).toEqual({ min: 1, max: 3 });
+    expect(minmaxColIndexArr([0, 1, 0, 0, 1, 0], 1)).toEqual({ min: 1, max: 4 });
+});
+
+test('minmaxRowIndexArr', () => {
+    expect(minmaxColIndexArr([0, 1, 0, 0, 1, 0], 1)).toEqual({ min: 1, max: 4 });
+    expect(minmaxColIndexArr([1, 1, 0, 0, 1, 0], 1)).toEqual({ min: 0, max: 4 });
+    expect(minmaxColIndexArr([1, 1, 5, 0, 1, 7], 1)).toEqual({ min: 0, max: 4 });
+    expect(minmaxColIndexArr([0, 1, 0, 0, 1, 0], 1)).toEqual({ min: 1, max: 4 });
 });
 
 test('minmaxColIndex', () => {
-    const m = reshape(filledBoard.map(x => x === 't' ? 't' : ' '), 10)
-    expect(minmaxColIndex(m, 't')).toEqual({ min: 1, max: 3 });
+    const m = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 1]]
+    expect(minmaxColIndex(m, 1)).toEqual({ min: 2, max: 4 });
 });
 
 test('minmaxRowIndex', () => {
-    const m = reshape(filledBoard.map(x => x === 't' ? 't' : ' '), 10)
-    expect(minmaxRowIndex(m, 't')).toEqual({ min: 2, max: 4 });
+    const m = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 1, 0], [0, 0, 0, 0, 0]]
+    expect(minmaxRowIndex(m, 1)).toEqual({ min: 1, max: 2 });
 });
 
 test('transpose matrix', () => {
@@ -79,9 +71,12 @@ test('matrix translate', () => {
     expect(translate(mat, 1, 2)).toEqual([[0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]]);
 })
 
-test('extract', () => { // symbols = ['f', 'i', 'l', 'n', 'p', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+test('extract 6x10', () => { // symbols = ['f', 'i', 'l', 'n', 'p', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const filledBoard = filledBoards['6x10'];
+    const pento = pentonimo(filledBoard)
+    const extract = pento.internals.extract
     const extr = (c) => {
-        const m = reshape(filledBoard.map(x => x === c ? c : ' '), 10)
+        const m = reshape(filledBoard.map(x => x === c ? c : ' '), pento.DIMC)
         return extract(rotate90(extract(m, c)), c)
     }
     expect(extr('f')).toEqual([[' ', 'f', ' '], ['f', 'f', 'f'], [' ', ' ', 'f']]);
@@ -98,16 +93,49 @@ test('extract', () => { // symbols = ['f', 'i', 'l', 'n', 'p', 't', 'u', 'v', 'w
     expect(extr('z')).toEqual([['z', ' ', ' '], ['z', 'z', 'z'], [' ', ' ', 'z']]);
 });
 
-test('generateTiles', () => {
-    const res = Object.entries(generateTiles()).map(([key, value]) => key + ":" + value.length)
+test('extract 4x15', () => { // symbols = ['f', 'i', 'l', 'n', 'p', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const filledBoard = filledBoards['4x15'];
+    const pento = pentonimo(filledBoard, 4, 15)
+    const extract = pento.internals.extract
+    const extr = (c) => {
+        const m = reshape(filledBoard.map(x => x === c ? c : ' '), pento.DIMC)
+        return extract(rotate90(extract(m, c)), c)
+    }
+    expect(extr('f')).toEqual([[' ', 'f', 'f'], ['f', 'f', ' '], [' ', 'f', ' ']]);
+    expect(extr('i')).toEqual([['i'], ['i'], ['i'], ['i'], ['i']]);
+    expect(extr('l')).toEqual([['l', ' ', ' ', ' '], ['l', 'l', 'l', 'l']]);
+    expect(extr('n')).toEqual([[' ', 'n'], ['n', 'n'], ['n', ' '], ['n', ' ']]);
+    expect(extr('p')).toEqual([[' ', 'p', 'p'], ['p', 'p', 'p']]);
+    expect(extr('t')).toEqual([[' ', ' ', 't'], ['t', 't', 't'], [' ', ' ', 't']]);
+    expect(extr('u')).toEqual([['u', 'u'], [' ', 'u'], ['u', 'u']]);
+    expect(extr('v')).toEqual([['v', 'v', 'v'], ['v', ' ', ' '], ['v', ' ', ' ']]);
+    expect(extr('w')).toEqual([['w', ' ', ' '], ['w', 'w', ' '], [' ', 'w', 'w']]);
+    expect(extr('x')).toEqual([[' ', 'x', ' '], ['x', 'x', 'x'], [' ', 'x', ' ']]);
+    expect(extr('y')).toEqual([[' ', 'y'], [' ', 'y'], ['y', 'y'], [' ', 'y']]);
+    expect(extr('z')).toEqual([[' ', 'z', 'z'], [' ', 'z', ' '], ['z', 'z', ' ']]);
+});
+
+test('generateTiles 6x10', () => {
+    const pento = pentonimo(filledBoards['6x10'])
+    const res = Object.entries(pento.internals.generateTiles()).map(([key, value]) => key + ":" + value.length)
     expect(res).toEqual(['f:64', 'i:56', 'l:248', 'n:248', 'p:304', 't:128', 'u:152', 'v:128', 'w:128', 'x:32', 'y:248', 'z:128']);
 })
 
-test('solvePentonimo', () => {
-    const solutions = solvePentonimo();
+test('solve pentonimo 6 x 10', () => {
+    const pento = pentonimo(filledBoards['6x10'])
+    const solutions = pento.solve();
     expect(solutions.length).toBe(2339)
-    expect(solutions.findIndex(s => s === 'llxiiiiiwwlxxxvvvwwylfxuuuvwyylffutuvzzyffnntpppzynnntttppzz')>=0).toBe(true)
-    expect(solutions.findIndex(s => s === 'llxtttyyyylxxxtppuyulfxwtppuuulffwwpzvvvffnnwwzzzvnnniiiiizv')>=0).toBe(true)
-    expect(solutions.findIndex(s => s === 'uuxllllvvvuxxxwwltnvuuxwwtttnvppfwzzytnnppffzyyyynpffzziiiii')>=0).toBe(true) 
+    expect(solutions.findIndex(s => s === 'llxiiiiiwwlxxxvvvwwylfxuuuvwyylffutuvzzyffnntpppzynnntttppzz') >= 0).toBe(true)
+    expect(solutions.findIndex(s => s === 'llxtttyyyylxxxtppuyulfxwtppuuulffwwpzvvvffnnwwzzzvnnniiiiizv') >= 0).toBe(true)
+    expect(solutions.findIndex(s => s === 'uuxllllvvvuxxxwwltnvuuxwwtttnvppfwzzytnnppffzyyyynpffzziiiii') >= 0).toBe(true)
+})
+
+test('solve pentonimo 4 x 15', () => {
+    const pento = pentonimo(filledBoards['4x15'], 4, 15)
+    const solutions = pento.solve();
+    expect(solutions.length).toBe(368)
+    // expect(solutions.findIndex(s => s === 'llxiiiiiwwlxxxvvvwwylfxuuuvwyylffutuvzzyffnntpppzynnntttppzz') >= 0).toBe(true)
+    // expect(solutions.findIndex(s => s === 'llxtttyyyylxxxtppuyulfxwtppuuulffwwpzvvvffnnwwzzzvnnniiiiizv') >= 0).toBe(true)
+    // expect(solutions.findIndex(s => s === 'uuxllllvvvuxxxwwltnvuuxwwtttnvppfwzzytnnppffzyyyynpffzziiiii') >= 0).toBe(true)
 })
 
