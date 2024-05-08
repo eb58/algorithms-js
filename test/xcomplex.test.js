e669b9aefe42410e7bbb22bdf9f7ac72da70b1f7
Erich Brandl <erich.brandl@gmail.com>
2024-05-08 08:46:49 +0200

    complex: reorganize tests

--- test/xcomplex.test.js
+++
@@ -1,9 +0,0 @@
-const { evalComplex, evalScalar } = require('../src/complex')
-
-test('evalScalar', () => {
-  // with variables and functions
-  const [a, b] = [3, 7]
-  // const vars = { a, b }
-  const vars = { a, b, f: (x) => x * x }
-  console.log( evalScalar('f(5)*f(5)', vars) ) 
-})
