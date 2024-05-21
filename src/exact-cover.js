b6d953ea7b7c9ffe672c0049238e8ed325848a8a
Erich Brandl <erich.brandl@gmail.com>
2024-05-21 09:12:43 +0200

    excat-cover: wip

--- src/exact-cover.js
+++ src/exact-cover.js
@@ -1,31 +1,38 @@
+const { f } = require('dlxlib');
+
 const { range, rangeFilled, zip, add } = require('./ol/ol').ol;
 const bitset = require('./ol/ol').bitset;
 
-const conditionIsOk = (cover, cond) => zip(cover, cond, add).every((x) => x <= 1);
+const noConflict = (cover, cond) => zip(cover, cond, add).every((x) => x <= 1);
+const cmpCond = (c1, c2) => (c1.join('') < c2.join('') ? 1 : -1)
 
 const solve = (conditions) => {
   // console.log(conditions);
   const solutions = [];
-  const solv = (conditions, cover, res) => {
-    // console.log('AAALEVEL', 'COVER', cover.join(''), res);
+
+  // conditions = conditions.toSorted(cmpCond)
+
+  console.log(conditions.map((c) => c.join('')));
+  const solv = (cover, conditions, res) => {
     if (cover.every((x) => x === 1)) {
-      // console.log('solution found', res);
       solutions.push(res);
       return;
     }
 
+    const firstIndexZero = cover.findIndex((v) => v === 0);
+    let newConditions; 
+
     conditions
-      .filter((condition) => conditionIsOk(cover, condition))
-      .forEach((condition, idx) =>
-        solv(
-          conditions.filter((_, i) => i > idx),
-          zip(cover, condition, add),
-          [...res, [...condition]],
-        ),
-      );
+      .filter((condition) => condition[firstIndexZero] === 1) // conditions, that fill first zero item in cover
+      .filter((condition) => noConflict(cover, condition)) // conditions, that do not conflict with cover
+      .forEach((condition, idx) => {
+        const newCover = zip(cover, condition, add);
+        newConditions = conditions.filter((_, i) => i !== idx);
+        return solv(newCover, newConditions, [...res, [...condition]]);
+      });
   };
 
-  solv(conditions, rangeFilled(conditions[0].length), []);
+  solv(rangeFilled(conditions[0].length), conditions, []);
   const res = solutions.map((solution) => solution.map((sol) => conditions.findIndex((condition) => condition.join('') === sol.join(''))));
   console.log('AAA', res);
   return res;
--- test/exact-cover.test.js
+++ test/exact-cover.test.js
@@ -1,14 +1,17 @@
 const solve = require('../src/exact-cover');
 
-const problem1 = [
+const cmpCond = (c1, c2) => (c1.join('') < c2.join('') ? 1 : -1)
+
+const conditions1 = [
   [1, 0, 0, 0],
   [0, 1, 1, 0],
   [1, 0, 0, 1],
   [0, 0, 1, 1],
   [0, 1, 0, 0],
   [0, 0, 1, 0],
-];
-const problem2 = [
+].toSorted(cmpCond);
+
+const conditions2 = [
     [0, 0, 1, 0, 1, 1, 0],
     [1, 0, 0, 1, 0, 0, 1],
     [0, 1, 1, 0, 0, 1, 0],
@@ -16,12 +19,12 @@ const problem2 = [
     [0, 1, 0, 0, 0, 0, 1],
     [0, 0, 0, 1, 1, 0, 1],
     [0, 0, 0, 1, 1, 0, 1],
-];
+].toSorted(cmpCond);
 
 test('exactCover 1', () => {
- expect(solve(problem1)).toEqual([[0, 3, 4], [1, 2], [2, 4, 5]])
+ expect(solve(conditions1)).toEqual([[ 0, 2 ], [ 0, 3, 5 ], [ 1, 3, 4 ]])
 })
 
 test('exactCover 2', () => {
-    expect(solve(problem2)).toEqual([[0, 3, 4]])
- })
+  expect(solve(conditions2)).toEqual([[1, 3, 4]])
+})
