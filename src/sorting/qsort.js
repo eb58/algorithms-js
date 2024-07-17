32d8acbd65374acac294c724b04d4b77f4778295
Erich Brandl <erich.brandl@gmail.com>
2024-07-17 10:52:09 +0200

    qsort.js: simple refactoring

--- src/sorting/qsort.js
+++ src/sorting/qsort.js
@@ -1,48 +1,38 @@
-const { groupBy } = require('../ol/ol').ol
+const { groupBy, feedX } = require('../ol/ol').ol;
 
 Array.prototype.rndElem = function () {
-  return this[Math.floor(Math.random() * this.length)]
-}
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+const qsort = (a) => (a.length <= 1 ? a : [...qsort(a.filter((n) => n < a[0])), ...a.filter((n) => n === a[0]), ...qsort(a.filter((n) => n > a[0]))]);
 
 const qsort1 = (arr, cmp) => {
-  if (arr.length <= 1) return arr
-  const piv = arr.rndElem()
-  const eq = arr.filter((n) => cmp(n, piv) === 0)
+  if (arr.length <= 1) return arr;
+  const piv = arr.rndElem();
+  const eq = arr.filter((n) => cmp(n, piv) === 0);
   const lt = qsort1(
     arr.filter((n) => cmp(n, piv) < 0),
     cmp
-  )
+  );
   const gt = qsort1(
     arr.filter((n) => cmp(n, piv) > 0),
     cmp
-  )
-  return [...lt, ...eq, ...gt]
-}
+  );
+  return [...lt, ...eq, ...gt];
+};
 
-const qsort2 = (a, cmp) =>
-  a.length <= 1
-    ? a
-    : [
-        ...qsort2(
-          a.filter((n) => cmp(n, a[0]) < 0),
-          cmp
-        ),
-        ...a.filter((n) => cmp(n, a[0]) === 0),
-        ...qsort2(
-          a.filter((n) => cmp(n, a[0]) > 0),
-          cmp
-        ),
-      ]
+const qsort2 = (arr, cmp) => {
+  const partition = (arr, piv, cmp) => arr.reduce((res, n) => (res[cmp(n, piv) + 1].push(n), res), [[], [], []]);
+  const p = partition(arr, arr.rndElem(), cmp);
+  return arr.length <= 1 ? arr : [...qsort2(p[0], cmp), ...p[1], ...qsort2(p[2], cmp)];
+};
 
-const qsort3 = (arr, cmp) => {
-  const partition = (arr, piv, cmp) => arr.reduce((res, n) => (res[cmp(n, piv) + 1].push(n), res), [[], [], []])
-  const p = partition(arr, arr.rndElem(), cmp)
-  return arr.length <= 1 ? arr : [...qsort3(p[0], cmp), ...p[1], ...qsort3(p[2], cmp)]
-}
+const qsort3 = (a, cmp) =>
+  feedX(a.rndElem(), (piv) =>
+    feedX(
+      groupBy(a, (n) => cmp(n, piv) + 1),
+      (p) => (a.length <= 1 ? a : [...qsort3(p[0] || [], cmp), ...(p[1] || []), ...qsort3(p[2] || [], cmp)])
+    )
+  );
 
-const qsort4 = (arr, cmp) => {
-  const p = groupBy(arr, (n) => cmp(n, arr.rndElem()) + 1)
-  return arr.length <= 1 ? arr : [...qsort4(p[0] || [], cmp), ...(p[1] || []), ...qsort4(p[2] || [], cmp)]
-}
-
-module.exports = [qsort1, qsort2, qsort3]
+module.exports = [qsort1, qsort2, qsort3];
