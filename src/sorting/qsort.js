b213160c171aa4679b72d9275de84d20e9943605
Erich Brandl <erich.brandl@gmail.com>
2024-07-19 12:22:46 +0200

    qsort.js: just refactoring

--- src/sorting/qsort.js
+++ src/sorting/qsort.js
@@ -1,38 +1,19 @@
-const { groupBy, feedX } = require('../ol/ol').ol;
-
-Array.prototype.rndElem = function () {
-  return this[Math.floor(Math.random() * this.length)];
-};
+const { feedX, randomElem } = require('../ol/ol').ol;
 
 const qsort = (a) => (a.length <= 1 ? a : [...qsort(a.filter((n) => n < a[0])), ...a.filter((n) => n === a[0]), ...qsort(a.filter((n) => n > a[0]))]);
 
-const qsort1 = (arr, cmp) => {
-  if (arr.length <= 1) return arr;
-  const piv = arr.rndElem();
-  const eq = arr.filter((n) => cmp(n, piv) === 0);
-  const lt = qsort1(
-    arr.filter((n) => cmp(n, piv) < 0),
-    cmp
-  );
-  const gt = qsort1(
-    arr.filter((n) => cmp(n, piv) > 0),
-    cmp
-  );
-  return [...lt, ...eq, ...gt];
+const qsort1 = (xs, cmp) => {
+  const lt = (xs, a) => xs.filter((x) => cmp(x, a) < 0);
+  const eq = (xs, a) => xs.filter((x) => cmp(x, a) === 0);
+  const gt = (xs, a) => xs.filter((x) => cmp(x, a) > 0);
+  const qs = (xs) => feedX(randomElem(xs), (piv) => (xs.length <= 1 ? xs : [...qs(lt(xs, piv)), ...eq(xs, piv), ...qs(gt(xs, piv))]));
+  return qs(xs);
 };
 
-const qsort2 = (arr, cmp) => {
-  const partition = (arr, piv, cmp) => arr.reduce((res, n) => (res[cmp(n, piv) + 1].push(n), res), [[], [], []]);
-  const p = partition(arr, arr.rndElem(), cmp);
-  return arr.length <= 1 ? arr : [...qsort2(p[0], cmp), ...p[1], ...qsort2(p[2], cmp)];
+const qsort2 = (xs, cmp) => {
+  const partition = (xs, piv) => xs.reduce((res, n) => (res[cmp(n, piv) + 1].push(n), res), [[], [], []]);
+  const qs = (xs) => feedX(partition(xs, randomElem(xs)), (p) => (xs.length <= 1 ? xs : [...qs(p[0]), ...p[1], ...qs(p[2])]));
+  return qs(xs);
 };
 
-const qsort3 = (a, cmp) =>
-  feedX(a.rndElem(), (piv) =>
-    feedX(
-      groupBy(a, (n) => cmp(n, piv) + 1),
-      (p) => (a.length <= 1 ? a : [...qsort3(p[0] || [], cmp), ...(p[1] || []), ...qsort3(p[2] || [], cmp)])
-    )
-  );
-
-module.exports = [qsort1, qsort2, qsort3];
+module.exports = [qsort1, qsort2];
