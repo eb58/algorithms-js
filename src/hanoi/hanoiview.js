ad95422e850668f5e1f39b8e87b3f05c7c202b19
Erich Brandl <erich.brandl@gmail.com>
2024-08-15 09:41:57 +0200

    hanoi: just design

--- src/hanoi/hanoiview.js
+++ src/hanoi/hanoiview.js
@@ -1,17 +1,17 @@
 const hanoiview = (N) => { // N = number of bricks
-   const [wBrick, hBrick, hPlate] = [150, 30, 10];
+   const [wBrick, hBrick, hPlate] = [150, 20, 10];
    const hPole = hBrick * (N + 1);
 
    const drawTower = (n, tower) => {
       const paper = Raphael(30 + n * (wBrick + 10), 15, wBrick, hPole + hPlate);
       paper.rect(0, 0, wBrick, hPole).attr({ fill: "lightgrey" }); // draw background
-      paper.rect(wBrick / 2 - 5, 5, 10, hPole).attr("fill", "black"); // pole
-      paper.rect(0, hPole, wBrick, hPlate).attr("fill", "black"); // bottom plate
+      paper.rect(wBrick / 2 - 5, 5, 10, hPole).attr("fill", "#173d10"); // pole
+      paper.rect(0, hPole, wBrick, hPlate).attr("fill", "#173d10"); // bottom plate
       tower.forEach((v, i) => { // draw bricks
          const w = v * wBrick / N;
          const left = (wBrick - w) / 2;
          const top = hPole - (i + 1) * hBrick;
-         paper.rect(left, top, w, hBrick).attr("fill", "red");
+         paper.rect(left, top, w, hBrick).attr("fill", "#77c468");
       });
    }
 
