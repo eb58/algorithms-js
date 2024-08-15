1032e55446ef3724bf89989b05d309b2a3a0adbc
Erich Brandl <erich.brandl@gmail.com>
2024-08-15 10:50:15 +0200

    hanoi.js: just some refactoring

--- src/hanoi/hanoi.js
+++ src/hanoi/hanoi.js
@@ -1,30 +1,41 @@
 const range = n => [...Array(n).keys()];
 const clone = o => JSON.parse(JSON.stringify(o));
+const tower = (n) => ({ l: range(n).reverse().map(x => x + 1), c: [], r: [] }); // { l:[1,...,n], c:[], r:[]} 
 
 const hanoi1 = (n) => {
    const h = (n, l, c, r) => {
       if (n === 0) return              // nothing to do
-      h(n - 1, l, r, c);               // move top n-1 element of left to center via right
-      r.push(l.pop());                 // move from left to right tower
-      result.push(clone({ l, c, r })); // keep track
-      h(n - 1, c, l, r);               // move top n-1 element of center to right via left
+      h(n - 1, l, r, c);               // move top n-1 elements of left to center via right
+      r.push(l.pop());                 // move brick from left to right tower
+      result.push(clone({ l, c, r })); // just keep track
+      h(n - 1, c, l, r);               // move top n-1 elements of center to right via left
    }
 
-   const tower = { l: range(n).reverse().map(x => x + 1), c: [], r: [] }; // { l:[1,...,n], c:[], r:[]} 
-   const result = [clone(tower)];
-   h(n, tower.l, tower.c, tower.r);
+   const t = tower(n)
+   const result = [clone(t)];
+   h(n, t.l, t.c, t.r);
    return result;
 };
 
 const hanoi2 = (() => {
-   initial = (xs) => xs.slice(0, - 1);
-   last = (xs) => xs[xs.length - 1];
-   const h = (n, l, c, r) => n === 0 ? [] : [...h(n - 1, l, r, c), [l, r], ...h(n - 1, c, l, r)]
 
-   moveTo = (m, from, to) => ({ ...clone(m), ...{ [to]: [...m[to], last(m[from])], [from]: initial(m[from]) } });
-   return (n) => h(n, 'l', 'c', 'r').reduce(
-      (res, move, idx) => [...res, moveTo(res[idx], move[0], move[1])],
-      [{ l: range(n).reverse().map(x => x + 1), c: [], r: [] }] // -> { l:[1,...,n], c:[], r:[] }
+   // h calculates all moves in notation "from -> to"
+   const h = (n, l, c, r) => n === 0 ? []  // nothing to do
+      : [
+         ...h(n - 1, l, r, c),             // move top n-1 elements of left to center via right
+         { from: l, to: r },               // just keep track of move 
+         ...h(n - 1, c, l, r)              // move top n-1 elements of center to right via left
+      ]
+
+   moveTo = (t, from, to) => ({
+      ...t,
+      [from]: t[from].slice(0, -1),           // take off top element of 'from'
+      [to]: [...t[to], ...t[from].slice(-1)]  // and put it on top of 'to'
+   });
+
+   return (n) => h(n, 'l', 'c', 'r').reduce((res, move, idx) =>
+      [...res, moveTo(res[idx], move.from, move.to)],
+      [tower(n)]
    );
 })()
 
