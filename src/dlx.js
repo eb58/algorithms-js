eddcd010426343d37125491b5e0167d98fd6b179
Erich Brandl <erich.brandl@gmail.com>
2025-02-12 10:24:38 +0100

    dlx + pentomino:fix error and some cleanup

--- src/dlx.js
+++ src/dlx.js
@@ -36,7 +36,7 @@ const colWithMinSize = (head) => {
   return c
 }
 
-const dlx_search = (head, solution, solutions, maxsolutions) => {
+const dlx_search = (head, solution, k, solutions, maxsolutions) => {
   if (head.right === head) {
     solutions.push([...solution])
     return solutions.length >= maxsolutions ? solutions : null
@@ -45,50 +45,53 @@ const dlx_search = (head, solution, solutions, maxsolutions) => {
   if (!c) return
   dlx_cover(c)
   for (let r = c.down; r !== c; r = r.down) {
-    solution.push(r.row)
+    solution[k] = r.row
     for (let j = r.right; j !== r; j = j.right) dlx_cover(j.column)
-    const s = dlx_search(head, solution, solutions, maxsolutions)
+    const s = dlx_search(head, solution, k + 1, solutions, maxsolutions)
     if (s != null) return s
     for (let j = r.left; j !== r; j = j.left) dlx_uncover(j.column)
   }
   dlx_uncover(c)
 }
 
-const dlx_solve = (matrix, maxsolutions = 1) => {
+const genSparseMatrix = (matrix) => {
   const RNGCOLS = range(matrix[0].length)
   const columns = RNGCOLS.map(() => ({ size: 0 }))
   columns.forEach((n, i) => (((n.up = n.down = n), (n.left = columns[i - 1])), (n.right = columns[i + 1])))
 
-  range(matrix.length).forEach((row) => {
+  for (let row = 0; row < matrix.length; row++) {
     let last = null
-    RNGCOLS.filter((col) => matrix[row][col]).forEach((col) => {
-      const node = {
-        row,
-        column: columns[col],
-        up: columns[col].up,
-        down: columns[col]
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col]) {
+        const node = {}
+        node.row = row
+        node.column = columns[col]
+        node.up = columns[col].up
+        node.down = columns[col]
+        if (last) {
+          node.left = last
+          node.right = last.right
+          last.right.left = node
+          last.right = node
+        } else {
+          node.left = node
+          node.right = node
+        }
+        columns[col].up.down = node
+        columns[col].up = node
+        columns[col].size++
+        last = node
       }
-      if (last) {
-        node.left = last
-        node.right = last.right
-        last.right.left = node
-        last.right = node
-      } else {
-        node.left = node
-        node.right = node
-      }
-      columns[col].up.down = node
-      columns[col].up = node
-      columns[col].size++
-      last = node
-    })
-  })
+    }
+  }
   const head = {
     right: columns[0],
     left: columns[columns.length - 1]
   }
   columns[0].left = columns[columns.length - 1].right = head
-  return dlx_search(head, [], [], maxsolutions)
+  return head
 }
+
+const dlx_solve = (matrix, maxsolutions) => dlx_search(genSparseMatrix(matrix), [], 0, [], maxsolutions)
 
 if (typeof module !== 'undefined') module.exports = dlx_solve
--- test/pentomino.test.js
+++ test/pentomino.test.js
@@ -1,14 +1,5 @@
-// const { DancingLinkX } = require('@algorithm.ts/dlx'); // do not understand how this works
-// const dlx = require('dlx'); // no solutions found in 5 min
-// const dlxlib = require('dlxlib'); // ~30 sec
-const dlx_solve = require('../src/dlx') // ~10.5 sec
-const dancingLinks = require('dancing-links') // ~9.5 sec
-
-// const solve = problem => dlxlib.solve(problem); // ~30 sec
-//const dlxSolve =  problem => dlx_solve(problem, 368 ); // ~10.5 sec
-const dlxSolve = (problem) => dancingLinks.findAll(problem.map((row) => ({ row }))).map((x) => x.map((o) => o.index))
-
 const pentomino = require('../src/pentomino/pentomino')
+const dlx_solve = require('../src/dlx') // ~10.5 sec
 
 test('symbols', () => {
   const pento = pentomino()
@@ -27,25 +18,23 @@ test('generateTiles 3x20', () => {
   expect(res).toEqual(['f:36', 'i:48', 'l:136', 'n:136', 'p:220', 't:72', 'u:110', 'v:72', 'w:72', 'x:18', 'y:136', 'z:72'])
 })
 
-const boardToString = (b) => b.flat().join('')
-
 test('solve pentomino 6x10', () => {
   const pento = pentomino()
+  const dlxSolve = (problem) => dlx_solve(problem, 2339) // ~10.5 sec
   const solutions = pento.solve(6, 10, dlxSolve)
   expect(solutions.length).toBe(2339)
-  // expect(solutions.findIndex((s) => boardToString(s) === 'llxiiiiiwwlxxxvvvwwylfxuuuvwyylffutuvzzyffnntpppzynnntttppzz') >= 0).toBe(true);
-  // expect(solutions.findIndex((s) => boardToString(s) === 'llxtttyyyylxxxtppuyulfxwtppuuulffwwpzvvvffnnwwzzzvnnniiiiizv') >= 0).toBe(true);
-  // expect(solutions.findIndex((s) => boardToString(s) === 'uuxllllvvvuxxxwwltnvuuxwwtttnvppfwzzytnnppffzyyyynpffzziiiii') >= 0).toBe(true);
 })
 
 test('solve pentomino 4x15', () => {
   const pento = pentomino()
+  const dlxSolve = (problem) => dlx_solve(problem, 368) // ~10.5 sec
   const solutions = pento.solve(4, 15, dlxSolve)
   expect(solutions.length).toBe(368)
 })
 
 test('solve pentomino 3x20', () => {
   const pento = pentomino()
+  const dlxSolve = (problem) => dlx_solve(problem, 2) // ~10.5 sec
   const solutions = pento.solve(3, 20, dlxSolve)
   expect(solutions.length).toBe(2)
 })
