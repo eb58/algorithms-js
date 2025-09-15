fb1d652e085c439f47e1b61e5c22551ea6d10abd
eb58 <erich.brandl@gmail.com>
2025-09-15 11:51:10 +0200

    complex.js + tokenizer.js:  - rename token -> symbol

--- src/complex.js
+++ src/complex.js
@@ -40,22 +40,22 @@ const C$ = (() => {
 
     const operand = () => {
       token = t.getToken()
-      if (token.token === tokens.minus) return cops.neg(operand())
-      if (token.token === tokens.plus) return operand()
-      if (token.token === tokens.number) return C$(token.value)
-      if (token.token === tokens.lparen) {
+      if (token.symbol === tokens.minus) return cops.neg(operand())
+      if (token.symbol === tokens.plus) return operand()
+      if (token.symbol === tokens.number) return C$(token.value)
+      if (token.symbol === tokens.lparen) {
         const ret = expression()
-        if (token.token !== tokens.rparen) throw Error(`Closing bracket not found!. Pos:${t.strpos()}`)
+        if (token.symbol !== tokens.rparen) throw Error(`Closing bracket not found!. Pos:${t.strpos()}`)
         return ret
       }
-      if (token.token === tokens.ident) {
+      if (token.symbol === tokens.ident) {
         const valOrFct = varsOrFcts[token.name]
         if (!valOrFct) throw Error(`Unknown identifier ${token.name}. Pos:${t.strpos()}`)
         if (typeof valOrFct !== 'function') return C$(valOrFct)
         token = t.getToken()
         const expressions = [expression()]
-        while (token.token === tokens.comma) expressions.push(expression())
-        if (token.token !== tokens.rparen) throw Error(`Closing bracket not found! Pos:${t.strpos()}`)
+        while (token.symbol === tokens.comma) expressions.push(expression())
+        if (token.symbol !== tokens.rparen) throw Error(`Closing bracket not found! Pos:${t.strpos()}`)
         return valOrFct(...expressions)
       }
       throw Error(`Operand expected. Pos:${t.strpos()}`)
@@ -64,29 +64,29 @@ const C$ = (() => {
     const term = () => {
       const val = operand()
       token = t.getToken()
-      return token.token !== tokens.pow ? val : cops.pow(val, term())
+      return token.symbol !== tokens.pow ? val : cops.pow(val, term())
     }
 
     const factor = () => {
       let val = term()
-      while (token.token === tokens.times || token.token === tokens.divide) {
-        if (token.token === tokens.times) val = cops.mul(val, term())
-        if (token.token === tokens.divide) val = cops.div(val, term())
+      while (token.symbol === tokens.times || token.symbol === tokens.divide) {
+        if (token.symbol === tokens.times) val = cops.mul(val, term())
+        if (token.symbol === tokens.divide) val = cops.div(val, term())
       }
       return val
     }
 
     const expression = () => {
       let val = factor()
-      while (token.token === tokens.plus || token.token === tokens.minus) {
-        if (token.token === tokens.plus) val = cops.add(val, factor())
-        if (token.token === tokens.minus) val = cops.sub(val, factor())
+      while (token.symbol === tokens.plus || token.symbol === tokens.minus) {
+        if (token.symbol === tokens.plus) val = cops.add(val, factor())
+        if (token.symbol === tokens.minus) val = cops.sub(val, factor())
       }
       return val
     }
 
     const val = expression()
-    if (token.token !== tokens.end) throw Error(`Unexpected symbol. Pos:${t.strpos()}`)
+    if (token.symbol !== tokens.end) throw Error(`Unexpected symbol. Pos:${t.strpos()}`)
     //** console.log('***', s, val, varsOrFcts, params || '' );
     return val
   }
--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,54 +1,71 @@
-const tokenStrings = Object.freeze(['ident', 'number', 'minus', 'plus', 'times', 'divide', 'pow', 'lparen', 'rparen', 'lbracket', 'rbracket', 'comma', 'end']);
-const tokens = Object.freeze(tokenStrings.reduce((acc, s) => ({ ...acc, [s]: s }), {}));
+const tokenStrings = Object.freeze([
+  'ident',
+  'number',
+  'minus',
+  'plus',
+  'times',
+  'divide',
+  'pow',
+  'lparen',
+  'rparen',
+  'lbracket',
+  'rbracket',
+  'comma',
+  'end'
+])
+const tokens = Object.freeze(tokenStrings.reduce((acc, s) => ({ ...acc, [s]: s }), {}))
 
 tokenizer = (input) => {
-    input = input.replace(/\s+/g, '')
-    let strpos = 0;
+  input = input.replace(/\s+/g, '')
+  let strpos = 0
 
-    const mapCharToToken = Object.freeze({
-        '+': tokens.plus,
-        '-': tokens.minus,
-        '*': tokens.times,
-        '/': tokens.divide,
-        '(': tokens.lparen,
-        ')': tokens.rparen,
-        '[': tokens.lbracket,
-        ']': tokens.rbracket,
-        '^': tokens.pow,
-        ',': tokens.comma
-    });
+  const mapCharToToken = Object.freeze({
+    '+': tokens.plus,
+    '-': tokens.minus,
+    '*': tokens.times,
+    '/': tokens.divide,
+    '(': tokens.lparen,
+    ')': tokens.rparen,
+    '[': tokens.lbracket,
+    ']': tokens.rbracket,
+    '^': tokens.pow,
+    ',': tokens.comma
+  })
 
-    const isLetter = (c) => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c === '_';
-    const isDigit = (c) => c >= '0' && c <= '9';
-    const isNumberChar = (c) => isDigit(c) || c === '.';
-    const isIdentifierChar = (c) => isLetter(c) || isDigit(c);
-    const getIdentOrNumber = (qualifier) => (qualifier(input[strpos]) ? input[strpos++] + getIdentOrNumber(qualifier) : '');
+  const isLetter = (c) => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c === '_'
+  const isDigit = (c) => c >= '0' && c <= '9'
+  const isNumberChar = (c) => isDigit(c) || c === '.'
+  const isIdentifierChar = (c) => isLetter(c) || isDigit(c)
+  const getIdentOrNumber = (qualifier) => (qualifier(input[strpos]) ? input[strpos++] + getIdentOrNumber(qualifier) : '')
 
-    const getIdentifier = () => ({
-        token: tokens.ident,
-        name: getIdentOrNumber(isIdentifierChar),
-    });
+  const getIdentifier = () => ({
+    symbol: tokens.ident,
+    name: getIdentOrNumber(isIdentifierChar)
+  })
 
-    const getNumber = () => ({
-        token: tokens.number,
-        value: parseFloat(getIdentOrNumber(isNumberChar))
-    });
+  const getNumber = () => ({
+    symbol: tokens.number,
+    value: parseFloat(getIdentOrNumber(isNumberChar))
+  })
 
-    return {
-        strpos: () => strpos,
-        getTokens: () => tokens,
-        getToken: () => {
-            if (strpos >= input.length) return { token: tokens.end };
+  return {
+    strpos: () => strpos,
+    getTokens: () => tokens,
+    getToken: () => {
+      if (strpos >= input.length) return { symbol: tokens.end }
 
-            const c = input[strpos];
-            if (isLetter(c)) return getIdentifier();
-            if (isDigit(c)) return getNumber();
-            if (c === '*' && input[strpos + 1] === '*') { strpos += 2; return ({ token: tokens.pow }) }
-            if (!mapCharToToken[c]) throw Error(`Char ${c} not allowed. Pos:${strpos}`);
-            if (strpos < input.length) strpos++;
-            return { token: mapCharToToken[c] };
-        }
-    };
-};
+      const c = input[strpos]
+      if (isLetter(c)) return getIdentifier()
+      if (isDigit(c)) return getNumber()
+      if (c === '*' && input[strpos + 1] === '*') {
+        strpos += 2
+        return { symbol: tokens.pow }
+      }
+      if (!mapCharToToken[c]) throw Error(`Char ${c} not allowed. Pos:${strpos}`)
+      if (strpos < input.length) strpos++
+      return { symbol: mapCharToToken[c] }
+    }
+  }
+}
 
-if (typeof module !== 'undefined') module.exports = tokenizer;
+if (typeof module !== 'undefined') module.exports = tokenizer
