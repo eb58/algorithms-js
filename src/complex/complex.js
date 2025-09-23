44ad232b1e3c39e501e9ffbc5bfa4d21a03dc85c
eb58 <erich.brandl@gmail.com>
2025-09-23 09:41:51 +0200

    complex.js: minor refactorings

--- src/complex/complex.js
+++ src/complex/complex.js
@@ -20,31 +20,31 @@ const ops = {
   [TOKENS.pow]: cops.pow
 }
 
-const unaryNode = (sign, op) => ({ eval: (vars) => (sign === TOKENS.minus ? cops.neg(op.eval(vars)) : op.eval(vars)) })
 const numberNode = (val) => ({ eval: () => (typeof val === 'number' ? { re: val, im: 0 } : val) })
+const unaryNode = (sign, op) => ({ eval: (vars) => (sign === TOKENS.minus ? cops.neg(op.eval(vars)) : op.eval(vars)) })
 const variableNode = (name) => ({ eval: (vars) => (typeof vars[name] === 'number' ? { re: vars[name], im: 0 } : vars[name]) })
 const functionNode = (name, params) => ({ eval: (vars) => globalScope[name](...params.reduce((acc, param) => [...acc, param.eval(vars)], [])) })
 const binaryOpNode = (op, left, right) => ({ eval: (vars) => ops[op](left.eval(vars), right.eval(vars)) })
 
 const parser = (s) => {
   const freeParams = new Set()
-  const {peek, consume} = tokenizer(s)
+  const { peek, consume } = tokenizer(s)
 
   const parseExpression = () => {
     let node = parseTerm()
-    while (peek()?.symbol === TOKENS.plus || peek()?.symbol === TOKENS.minus) node = binaryOpNode(consume().symbol, node, parseTerm())
+    while (peek().symbol === TOKENS.plus || peek().symbol === TOKENS.minus) node = binaryOpNode(consume().symbol, node, parseTerm())
     return node
   }
 
   const parseTerm = () => {
     let node = parseFactor()
-    while (peek()?.symbol === TOKENS.times || peek()?.symbol === TOKENS.divide) node = binaryOpNode(consume().symbol, node, parseFactor())
+    while (peek().symbol === TOKENS.times || peek().symbol === TOKENS.divide) node = binaryOpNode(consume().symbol, node, parseFactor())
     return node
   }
 
   const parseFactor = () => {
     let node = parseOperand()
-    while (peek()?.symbol === TOKENS.pow) node = binaryOpNode(consume().symbol, node, parseFactor())
+    while (peek().symbol === TOKENS.pow) node = binaryOpNode(consume().symbol, node, parseFactor())
     return node
   }
 
@@ -64,7 +64,7 @@ const parser = (s) => {
         const funcName = consume().name
         if (consume().symbol !== TOKENS.lparen) throw new Error(`Opening paren expected` + peek())
         const expressions = [parseExpression()]
-        while (peek()?.symbol === TOKENS.comma) {
+        while (peek().symbol === TOKENS.comma) {
           consume()
           expressions.push(parseExpression())
         }
@@ -76,13 +76,13 @@ const parser = (s) => {
     if (token.symbol === TOKENS.lparen) {
       consume()
       const node = parseExpression()
-      if (peek()?.symbol !== TOKENS.rparen) throw new Error(`Closing bracket not found!. Pos:${token.strpos}`)
+      if (peek().symbol !== TOKENS.rparen) throw new Error(`Closing bracket not found!. Pos:${token.strpos}`)
       consume()
       return node
     }
     throw new Error(`Operand expected. Pos:${token.strpos}`)
   }
-  const getParameters = () => Array.from(freeParams)
+  const getParameters = () => Array.from(freeParams).filter((x) => !(x in globalScope))
 
   return {
     parseExpression,
@@ -91,13 +91,13 @@ const parser = (s) => {
 }
 
 const C$ = (param1, param2) => {
-  if (typeof param1 === 'number') return { re: param1 || 0, im: param2 || 0 }
-  if (typeof param1 === 'object' && Object.keys(param1).every((k) => k === 're' || k === 'im')) return { re: 0, im: 0, ...param1 }
-  if (typeof param1 === 'string') {
+  if (typeof param1 === 'number') return { re: param1 || 0, im: param2 || 0 } // C$(1, 1)
+  if (typeof param1 === 'object' && Object.keys(param1).every((k) => k === 're' || k === 'im')) return { re: 0, im: 0, ...param1 } // C$({ re: 1, im: 1 })
+  if (typeof param1 === 'string') {  // C$("3+i") ->
     globalScope = { ...globalScope, ...param2 }
     const p = parser(param1)
     const ast = p.parseExpression()
-    const vars = p.getParameters().filter((x) => !(x in globalScope))
+    const vars = p.getParameters()
 
     return vars.length === 0
       ? ast.eval(param2)
