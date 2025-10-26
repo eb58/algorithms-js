b192479e126f642ec995ef88f347912a519a25ee
eb58 <erich.brandl@gmail.com>
2025-10-26 08:49:35 +0100

    mandelbrot: minor refactorings

--- src/mandelbrot/mandelbrot.worker.js
+++ src/mandelbrot/mandelbrot.worker.js
@@ -9,40 +9,20 @@ const getColor = (n, maxIterations) => {
 
   const ratio = n / maxIterations
   const hue = ratio * 360
-  const saturation = 100
   const lightness = ratio < 0.5 ? 100 * ratio : 50
 
   const h = hue / 60
-  const c = ((1 - Math.abs((2 * lightness) / 100 - 1)) * saturation) / 100
+  const c = 1 - Math.abs((2 * lightness) / 100 - 1)
   const x = c * (1 - Math.abs((h % 2) - 1))
   const m = lightness / 100 - c / 2
 
   let r, g, b
-  if (h < 1) {
-    r = c
-    g = x
-    b = 0
-  } else if (h < 2) {
-    r = x
-    g = c
-    b = 0
-  } else if (h < 3) {
-    r = 0
-    g = c
-    b = x
-  } else if (h < 4) {
-    r = 0
-    g = x
-    b = c
-  } else if (h < 5) {
-    r = x
-    g = 0
-    b = c
-  } else {
-    r = c
-    g = 0
-    b = x
-  }
+  if (h < 1) [r, g, b] = [c, x, 0]
+  else if (h < 2) [r, g, b] = [x, c, 0]
+  else if (h < 3) [r, g, b] = [0, c, x]
+  else if (h < 4) [r, g, b] = [0, x, c]
+  else if (h < 5) [r, g, b] = [x, 0, c]
+  else [r, g, b] = [c, 0, x]
   return [Math.round((r + m) * 255), Math.round((g + m) * 255), Math.round((b + m) * 255), 255]
 }
 
@@ -51,8 +31,8 @@ let getCol = getColor
 const mandelbrot = (cx, cy, maxIterations = 100) => {
   let [x, y, n] = [cx, cy, 0]
   if ((x + 0.25) * (x + 0.25) + y * y < 0.25) return maxIterations
-  if ((x +1) * (x + 1) + y * y < 0.05) return maxIterations
-  // eleganter aber langsamer ! while (n++ < maxIterations && x * x + y * y <= 4) [x, y] = [x * x - y * y + cx, 2 * x * y + cy]
+  if ((x + 1) * (x + 1) + y * y < 0.05) return maxIterations
+  // eleganter aber etwas langsamer: while (n++ < maxIterations && x * x + y * y <= 4) [x, y] = [x * x - y * y + cx, 2 * x * y + cy]
   while (n++ < maxIterations && x * x + y * y <= 4) {
     const xTemp = x * x - y * y + cx
     y = 2 * x * y + cy
